594f98038c0c065a0eda62e6d1aa32facaf1ab96
IceDBorn <fundwork@tutanota.com>
2021-07-10 10:55:41 +0300

    Check if applications exist before prompting user for input

--- install.sh
+++ install.sh
@@ -7,8 +7,27 @@ username=$(whoami)
 sudo chmod +x apps-configs/install-apps.sh zsh/install-zsh-plugins.sh  single-gpu-passthrough/install-single-gpu-passthrough.sh
 
 # Install needed applications
-sudo pacman -S git
-yay -S sublime-text-4
+if ! command -v git &> /dev/null
+then
+    echo "Git is not installed"
+    echo "Installing..."
+    sudo pacman -S git
+    exit
+else
+  echo "Git is installed"
+  echo "Skipping..."
+fi
+
+if ! command -v subl &> /dev/null
+then
+    echo "Sublime Text is not installed"
+    echo "Installing..."
+    yay -S sublime-text-4
+    exit
+else
+  echo "Sublime Text is installed"
+  echo "Skipping..."
+fi
 
 # Generic applications installer
 while true
@@ -50,56 +69,71 @@ do
 done
 
 # Startup items
-while true
-do
-	read -r -p "Do you want to add GreenWithEnvy to startup? [y/n] " input
+if command -v gwe &> /dev/null
+then
+    echo "GreenWithEnvy is installed..."
+    while true
+    do
+      read -r -p "Do you want to add GreenWithEnvy to startup? [y/n] " input
 
- 	case $input in [yY][eE][sS]|[yY])
-		mv panel-keyboard-startup-appmenu-settings/GreenWithEnvy.desktop /home/"$username"/.config/autostart/
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+      case $input in [yY][eE][sS]|[yY])
+        mv panel-keyboard-startup-appmenu-settings/GreenWithEnvy.desktop /home/"$username"/.config/autostart/
+        break
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
-
-while true
-do
-	read -r -p "Do you want to add ksuperkey to startup? [y/n] " input
-
- 	case $input in [yY][eE][sS]|[yY])
-		mv panel-keyboard-startup-appmenu-settings/ksuperkey.desktop /home/"$username"/.config/autostart/
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+      esac
+    done
+    exit
+fi
 
-while true
-do
-	read -r -p "Do you want to add Discord Canary to startup? [y/n] " input
+if command -v ksuperkey &> /dev/null
+then
+    echo "ksuperkey is installed..."
+    while true
+    do
+      read -r -p "Do you want to add ksuperkey to startup? [y/n] " input
 
- 	case $input in [yY][eE][sS]|[yY])
-		mv panel-keyboard-startup-appmenu-settings/Powercord.desktop /home/"$username"/.config/autostart/
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+      case $input in [yY][eE][sS]|[yY])
+        mv panel-keyboard-startup-appmenu-settings/ksuperkey.desktop /home/"$username"/.config/autostart/
+        break
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
+        ;;
+      esac
+    done
+    exit
+fi
+
+if command -v discord-canary &> /dev/null
+then
+    echo "Discord Canary is installed..."
+    while true
+    do
+      read -r -p "Do you want to add Discord Canary to startup? [y/n] " input
+
+      case $input in [yY][eE][sS]|[yY])
+        mv panel-keyboard-startup-appmenu-settings/Powercord.desktop /home/"$username"/.config/autostart/
+        break
+        ;;
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
+        ;;
+      esac
+    done
+    exit
+fi
 
 while true
 do
@@ -119,152 +153,160 @@ do
 done
 
 # Zsh installer
-while true
-do
-	read -r -p "Do you want to install zsh? [y/n] " input
- 
- 	case $input in [yY][eE][sS]|[yY])
+if ! command -v zsh &> /dev/null
+then
+    echo "zsh is not installed"
+    while true
+    do
+      read -r -p "Do you want to install zsh? [y/n] " input
 
-		sudo pacman -S zsh
+      case $input in [yY][eE][sS]|[yY])
 
-		while true
-		do
-			read -r -p "Do you want to install zsh plugins? [y/n] " input
- 
- 			case $input in [yY][eE][sS]|[yY])
-				while true
-				do
-					read -r -p "Do you want to edit the zsh plugins script? [y/n] " input
- 
- 					case $input in [yY][eE][sS]|[yY])
-						echo Press any button when you have finished editing the zsh plugins script...
-						read -r -n 1 -s
-						subl /zsh/install-apps.sh
-						echo Installing zsh plugins...
- 						./zsh/install-zsh-plugins.sh
- 						break
- 						;;
- 					[nN][oO]|[nN])
-						echo Installing zsh plugins...
- 						./zsh/install-zsh-plugins.sh
- 						break
-        				;;
-    				*)
- 						echo "Invalid input..."
- 						;;
- 					esac
-				done
-				echo Do not forget to add installed scripts to the zsh config file
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+        sudo pacman -S zsh
 
-		while true
-		do
-			read -r -p "Make zsh the default shell for root? [y/n] " input
- 
- 			case $input in [yY][eE][sS]|[yY])
-				sudo chsh -s /bin/zsh root
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+        while true
+        do
+          read -r -p "Do you want to install zsh plugins? [y/n] " input
 
-		while true
-		do
-			read -r -p "Make zsh the default shell for $username? [y/n] " input
- 
- 			case $input in [yY][eE][sS]|[yY])
-				sudo chsh -s /bin/zsh "$username"
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+          case $input in [yY][eE][sS]|[yY])
+            while true
+            do
+              read -r -p "Do you want to edit the zsh plugins script? [y/n] " input
 
-		while true
-		do
-			read -r -p "Do you want to install provided zsh's theme? [y/n] " input
- 
- 			case $input in [yY][eE][sS]|[yY])
-				mv zsh/promptline.sh ~/.promptline.sh
- 				echo Do not forget to add "'source ~/.promptline.sh'" and "'unsetopt PROMPT_SP'" into the zsh config file!
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+              case $input in [yY][eE][sS]|[yY])
+                echo Press any button when you have finished editing the zsh plugins script...
+                read -r -n 1 -s
+                subl /zsh/install-apps.sh
+                echo Installing zsh plugins...
+                ./zsh/install-zsh-plugins.sh
+                break
+                ;;
+              [nN][oO]|[nN])
+                echo Installing zsh plugins...
+                ./zsh/install-zsh-plugins.sh
+                break
+                    ;;
+                *)
+                echo "Invalid input..."
+                ;;
+              esac
+            done
+            echo Do not forget to add installed scripts to the zsh config file
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
 
-		while true
-		do
-			read -r -p "Do you want to edit zsh's config? [y/n] " input
- 
- 			case $input in [yY][eE][sS]|[yY])
+        while true
+        do
+          read -r -p "Make zsh the default shell for root? [y/n] " input
 
-				while true
-				do
-					read -r -p "Do you want to edit view recommended additions to the zsh config? [y/n] " input
- 
- 					case $input in [yY][eE][sS]|[yY])
-						subl zsh/zsh-config-additions.txt
- 						break
- 						;;
- 					[nN][oO]|[nN])
- 						break
-        				;;
-    				*)
- 						echo "Invalid input..."
- 						;;
- 					esac
-				done
+          case $input in [yY][eE][sS]|[yY])
+            sudo chsh -s /bin/zsh root
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
 
-				echo Press any button when you have finished editing the zsh config...
-				subl /home/"$username"/.zshrc
-				read -r -n 1 -s
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+        while true
+        do
+          read -r -p "Make zsh the default shell for $username? [y/n] " input
 
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+          case $input in [yY][eE][sS]|[yY])
+            sudo chsh -s /bin/zsh "$username"
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
+
+        while true
+        do
+          read -r -p "Do you want to install provided zsh's theme? [y/n] " input
+
+          case $input in [yY][eE][sS]|[yY])
+            mv zsh/promptline.sh ~/.promptline.sh
+            echo Do not forget to add "'source ~/.promptline.sh'" and "'unsetopt PROMPT_SP'" into the zsh config file!
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
+
+        while true
+        do
+          read -r -p "Do you want to edit zsh's config? [y/n] " input
+
+          case $input in [yY][eE][sS]|[yY])
+
+            while true
+            do
+              read -r -p "Do you want to edit view recommended additions to the zsh config? [y/n] " input
+
+              case $input in [yY][eE][sS]|[yY])
+                subl zsh/zsh-config-additions.txt
+                break
+                ;;
+              [nN][oO]|[nN])
+                break
+                    ;;
+                *)
+                echo "Invalid input..."
+                ;;
+              esac
+            done
+
+            echo Press any button when you have finished editing the zsh config...
+            subl /home/"$username"/.zshrc
+            read -r -n 1 -s
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
+
+        break
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
+        ;;
+      esac
+    done
+    exit
+else
+  echo "Zsh is installed"
+  echo "Skipping..."
+fi
 
 # Movescreen python script installer
 while true
@@ -286,108 +328,126 @@ do
 done
 
 # Kitty config installer
-while true
-do
-	read -r -p "Do you want to install kitty? [y/n] " input
+if ! command -v git &> /dev/null
+then
+    while true
+    do
+      read -r -p "Do you want to install kitty? [y/n] " input
 
-	case $input in [yY][eE][sS]|[yY])
-		sudo pacman -S kitty
+      case $input in [yY][eE][sS]|[yY])
+        sudo pacman -S kitty
 
-		while true
-		do
-			read -r -p "Do you want to install kitty's provided config? [y/n] " input
+        while true
+        do
+          read -r -p "Do you want to install kitty's provided config? [y/n] " input
 
-			case $input in [yY][eE][sS]|[yY])
- 				mv apps-configs/kitty.conf ~/.config/kitty
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+          case $input in [yY][eE][sS]|[yY])
+            mv apps-configs/kitty.conf ~/.config/kitty
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
+        break
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
+        ;;
+      esac
+    done
+    exit
+else
+  echo "Kitty is installed"
+  echo "Skipping..."
+fi
 
 # Powercord installer
-while true
-do
-	read -r -p "Do you want to install powercord? [y/n] " input
+if command -v discord-canary &> /dev/null
+then
+  echo "Discord Canary is installed..."
+  while true
+  do
+    read -r -p "Do you want to install powercord? [y/n] " input
 
-	case $input in [yY][eE][sS]|[yY])
-		mkdir ~/Projects
-		git clone https://github.com/powercord-org/powercord ~/Projects
-		(cd ~/Projects/powercord && npm i && npm run plug)
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
-        ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+    case $input in [yY][eE][sS]|[yY])
+      mkdir ~/Projects
+      git clone https://github.com/powercord-org/powercord ~/Projects
+      (cd ~/Projects/powercord && npm i && npm run plug)
+      break
+      ;;
+    [nN][oO]|[nN])
+      break
+          ;;
+      *)
+      echo "Invalid input..."
+      ;;
+    esac
+  done
+fi
 
 # Samba installer
-while true
-do
-	read -r -p "Do you want to install samba? [y/n] " input
+if ! command -v git &> /dev/null
+then
+    while true
+    do
+      read -r -p "Do you want to install samba? [y/n] " input
 
-	case $input in [yY][eE][sS]|[yY])
-		sudo pacman -S samba
-		sudo mv /etc/samba/smb.conf.arcolinux /etc/samba/smb.conf
+      case $input in [yY][eE][sS]|[yY])
+        sudo pacman -S samba
+        sudo mv /etc/samba/smb.conf.arcolinux /etc/samba/smb.conf
 
-		while true
-		do
-			read -r -p "Do you want to edit samba's config? [y/n] " input
+        while true
+        do
+          read -r -p "Do you want to edit samba's config? [y/n] " input
 
-			case $input in [yY][eE][sS]|[yY])
+          case $input in [yY][eE][sS]|[yY])
 
-				echo Press any button when you have finished editing the samba config...
-				sudo subl /etc/samba/smb.conf
-				read -r -n 1 -s
-				sudo smbpasswd -a "$username"
-				sudo systemctl disable samba
-				sudo systemctl stop samba
-				sudo systemctl enable smb
-				sudo systemctl start smb
-				sudo systemctl status smb
-				sudo systemctl enable nmb
-				sudo systemctl start nmb
-				sudo systemctl status nmb
- 				break
- 				;;
- 			[nN][oO]|[nN])
- 				break
-        		;;
-    		*)
- 				echo "Invalid input..."
- 				;;
- 			esac
-		done
+            echo Press any button when you have finished editing the samba config...
+            sudo subl /etc/samba/smb.conf
+            read -r -n 1 -s
+            sudo smbpasswd -a "$username"
+            sudo systemctl disable samba
+            sudo systemctl stop samba
+            sudo systemctl enable smb
+            sudo systemctl start smb
+            sudo systemctl status smb
+            sudo systemctl enable nmb
+            sudo systemctl start nmb
+            sudo systemctl status nmb
+            break
+            ;;
+          [nN][oO]|[nN])
+            break
+                ;;
+            *)
+            echo "Invalid input..."
+            ;;
+          esac
+        done
 
- 		break
- 		;;
- 	[nN][oO]|[nN])
- 		break
+        break
         ;;
-    *)
- 		echo "Invalid input..."
- 		;;
- 	esac
-done
+      [nN][oO]|[nN])
+        break
+            ;;
+        *)
+        echo "Invalid input..."
+        ;;
+      esac
+    done
+    exit
+else
+  echo "Samba is installed"
+  echo "Skipping..."
+fi
 
 # Pictures mover
 while true
@@ -409,7 +469,7 @@ do
  	esac
 done
 
-# Pictures mover
+# Enable ssh
 while true
 do
 	read -r -p "Do you want to enable ssh? [y/n] " input
