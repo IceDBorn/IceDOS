be3e8dbf6dad9767f57c4a7865c4f129f737ce2f
IceDBorn <github.envenomed@dralias.com>
2021-09-29 04:04:04 +0300

    Add '\n' to some echoes

--- setup.sh
+++ setup.sh
@@ -5,15 +5,15 @@ username=$(whoami)
 echo "Hello $username!"
 
 # Make child scripts executable
-echo "Marking child scripts as executables..."
+echo "\nMarking child scripts as executables..."
 sudo chmod +x apps/install.sh kde-theme/setup.sh photoshop/setup.sh single-gpu-passthrough/setup.sh zsh/plugins.sh
 
 # Applications installer
-echo "Installing applications..."
+echo "\nInstalling applications..."
 ./apps/install.sh
 
 # Zsh installer
-echo "Setting zsh as the default shell..."
+echo "\nSetting zsh as the default shell..."
 sudo chsh -s /bin/zsh root
 sudo chsh -s /bin/zsh "$username"
 
@@ -35,24 +35,24 @@ mv ~/.zshrc ~/.zshrc.old
 mv ~/.zshrc.new ~/.zshrc
 
 # Kitty config installer
-echo "Installing kitty config..."
+echo "\nInstalling kitty config..."
 cp apps/kitty.conf ~/.config/kitty/kitty.conf
 
 # Pictures mover
-echo "Adding pictures to the Pictures directory..."
+echo "\nAdding pictures to the Pictures directory..."
 cp pictures/arcolinux-hello.png ~/Pictures/.arcolinux-hello.png
 cp pictures/wallpaper.png ~/Pictures/.wallpaper.png
 
 # Enable ssh
-echo "Enabling SSH..."
+echo "\nEnabling ssh..."
 sudo systemctl enable sshd
 
 # Single gpu passthrough setup
-echo "Setting up Single GPU Passthrough..."
+echo "\nSetting up Single GPU Passthrough..."
 ./single-gpu-passthrough/setup.sh
 
 # nvm installer
-echo "Installing nvm"
+echo "\nInstalling nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Theme installer
