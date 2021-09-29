5c8805042206cd5638112411c23e55a052806cdd
IceDBorn <github.envenomed@dralias.com>
2021-09-29 17:19:16 +0300

    Add autostart items

--- settings/settings.sh
+++ settings/settings.sh
@@ -2,3 +2,7 @@
 
 # Clicking on files or folders selects them instead of opening them
 sed -i '/KDE/a SingleClick=false' ~/.config/kdeglobals
+
+# Add autostart items
+rm -rfv ~/.config/autostart/*
+cp -a settings/autostart ~/.config/autostart
