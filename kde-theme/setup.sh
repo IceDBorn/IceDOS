47f3ca3e88f79a88bc0203eb77991b62f8dd616a
IceDBorn <github.envenomed@dralias.com>
2021-10-01 04:22:21 +0300

    Add custom grub theme

--- kde-theme/setup.sh
+++ kde-theme/setup.sh
@@ -12,6 +12,12 @@ git clone https://gitlab.com/pwyde/monochrome-kde.git
 cd monochrome-kde || exit
 ./install.sh -i
 
+echo "Installing grub 2 themes..."
+git clone https://github.com/vinceliuice/grub2-themes.git
+cd grub2-themes || exit
+# -t [tela/vimix/stylish/slaze/whitesur] -i [color/white/whitesur] -s [1080p/2k/4k/ultrawide/ultrawide2k]
+sudo ./install.sh -t vimix -i white -s 1080p
+
 echo "Setting custom theme..."
 lookandfeeltool -a com.github.vinceliuice.Fluent-round-dark
 /usr/lib/plasma-changeicons Papirus-Dark
