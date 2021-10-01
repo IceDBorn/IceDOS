a76900d653fdd258dc9f6896977360a1a5037b18
IceDBorn <github.envenomed@dralias.com>
2021-10-01 07:54:44 +0300

    Add asian font support

--- apps/apps.sh
+++ apps/apps.sh
@@ -3,6 +3,17 @@
 # Add packages to their corresponding array
 pacman=(
 "--needed git base-devel yay"
+"adobe-source-han-sans-cn-fonts"
+"adobe-source-han-sans-hk-fonts"
+"adobe-source-han-sans-jp-fonts"
+"adobe-source-han-sans-kr-fonts"
+"adobe-source-han-sans-otc-fonts"
+"adobe-source-han-sans-tw-fonts"
+"adobe-source-han-serif-cn-fonts"
+"adobe-source-han-serif-jp-fonts"
+"adobe-source-han-serif-kr-fonts"
+"adobe-source-han-serif-otc-fonts"
+"adobe-source-han-serif-tw-fonts"
 "bluedevil"
 "bluez-utils"
 "bpytop"
