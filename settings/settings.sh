f3da49ffe44d13461c15d0200701c32400949c3e
IceDBorn <github.envenomed@dralias.com>
2021-10-03 04:12:15 +0300

    Maximize nvidia GPU power limit

--- settings/settings.sh
+++ settings/settings.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-username=$(whoami)
-
 # Clicking on files or folders selects them instead of opening them
+echo "Editing single click behavior..."
 sed -i '/KDE/a SingleClick=false' ~/.config/kdeglobals
 
 # Enables bluetooth headphones
+echo "Enabling headphones support for bluetooth..."
 sed -i '/General/a Enable=Source,Sink,Media,Socket' /etc/bluetooth/main.conf
 
 # Add autostart items
+echo "Adding autostart items..."
 cp -a settings/autostart ~/.config/
 
 # Remove guest account
+echo "Removing guest account..."
 sudo pacman -Rd systemd-guest-user
 
-# Enable sunshine service
-systemctl --machine="$username"@.host --user enable sunshine
-
 # Auto mount disks on startup
+echo "Adding mounts to fstab..."
 cat /etc/fstab settings/fstab > ~/.fstab.new
 sudo mv /etc/fstab /etc/fstab.old
 sudo mv ~/.fstab.new /etc/fstab
 
 # Install proton ge
+echo "Installing Proton GE..."
 sudo pip3 install protonup
 protonup -d ~/.steam/root/compatibilitytools.d/
 protonup -y
+
+# Maximize nvidia GPU power limit on startup
+echo "Maximizing GPU power limit..."
+sudo cp settings/nv-power-limit.sh /usr/local/sbin/nv-power-limit.sh
+sudo chmod 744 /usr/local/sbin/nv-power-limit.sh
+sudo mkdir /usr/local/etc/systemd
+sudo cp settings/nv-power-limit.service /usr/local/etc/systemd/nv-power-limit.service
+sudo chmod 644 /usr/local/etc/systemd/nv-power-limit.service
+sudo ln -s /usr/local/etc/systemd/nv-power-limit.service /etc/systemd/system/nv-power-limit.service
+sudo systemctl start nv-power-limit.service
+sudo systemctl enable nv-power-limit.service
