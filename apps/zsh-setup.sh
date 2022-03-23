5b757dec6d4c8052549cca0d2f24fad3291ec09b
icedborn <github.envenomed@dralias.com>
2022-03-23 19:34:27 +0200

    Simplify zsh personal config installation

--- apps/zsh-setup.sh
+++ apps/zsh-setup.sh
@@ -2,8 +2,8 @@
 
 # Set zsh as the default shell
 echo "Setting zsh as the default shell..."
-sudo chsh -s /bin/zsh root
-sudo chsh -s /bin/zsh "$username"
+sudo chsh -s /usr/bin/zsh root
+sudo chsh -s /usr/bin/zsh "$username"
 
 # Install zsh theme
 echo "Installing zsh theme..."
@@ -22,12 +22,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Installing Oh My Zsh plugins..."
 bash ./apps/zsh/install-zsh-plugins.sh
 
-# Append custom config to zsh config file
-echo "Adding custom config to '~/.zshrc'..."
-cat ~/.zshrc apps/zsh/zsh-config-append-content.txt > ~/.zshrc.new
-mv ~/.zshrc ~/.zshrc.old
-mv ~/.zshrc.new ~/.zshrc
+# Add personal zsh config to zshrc
+echo "Adding personal zsh config to '~/.zshrc'..."
+echo "source ~/.config/zsh/zsh-personal.sh" >> ~/.zshrc
 sed -i 's/^plugins=(\(.*\)/plugins=(archlinux npm nvm sudo systemd zsh-autosuggestions zsh-better-npm-completion zsh-syntax-highlighting \1/' ~/.zshrc
-
-# Revert to zsh config template before adding the firefox profile path
-mv apps/zsh/zsh-config-append-content.txt.old zsh/zsh-config-append-content.txt
