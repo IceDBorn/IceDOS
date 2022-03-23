d3d59ea3c80ba221b8c2b2bcbb4420a0aeac5f97
icedborn <github.envenomed@dralias.com>
2022-03-23 19:31:54 +0200

    Remove unnecessary code

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -4,6 +4,3 @@ echo "Installing Photoshop..."
 git clone https://github.com/Gictorbit/photoshopCClinux.git
 (cd photoshopCClinux && sudo chmod +x setup.sh && ./setup.sh)
 rm -rf photoshopCClinux
-
-echo "Removing post install script..."
-rm -rf ~/post-install.sh ~/.config/autostart/post-install.desktop
