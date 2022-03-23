92a48fe044ce77ef975ad4db61033c159a3a62b6
icedborn <github.envenomed@dralias.com>
2022-03-23 17:50:06 +0200

    Remove chaotic aur

--- apps/install-apps.sh
+++ apps/install-apps.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Install Chaotic AUR as a pacman mirror
-echo "Installing Chaotic AUR..."
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+username=$(whoami)
 
 # Enable multilib
 echo "[multilib]" | sudo tee -a /etc/pacman.conf
