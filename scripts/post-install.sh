437941daf084da4d65b430fa60b714f6d604ec26
icedborn <github.envenomed@dralias.com>
2022-04-12 04:45:58 +0300

    Set wallpaper post install

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -4,3 +4,5 @@ echo "Installing Photoshop..."
 git clone https://github.com/Gictorbit/photoshopCClinux.git
 (cd photoshopCClinux && sudo chmod +x setup.sh && ./setup.sh)
 rm -rf photoshopCClinux
+
+gsettings set org.gnome.desktop.background picture-uri file:///home/"$username"/Pictures/.wallpaper.png
