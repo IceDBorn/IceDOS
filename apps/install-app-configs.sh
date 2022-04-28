245a6435258dd27853de1c9553bc88e41e64d891
icedborn <github.envenomed@dralias.com>
2022-04-28 23:15:26 +0300

    Remove flameshot config installer

--- apps/install-app-configs.sh
+++ apps/install-app-configs.sh
@@ -5,11 +5,6 @@ echo "Installing kitty config..."
 mkdir -p ~/.config/kitty/
 cp apps/configs/kitty.conf ~/.config/kitty/kitty.conf
 
-# Flameshot config installer
-echo "Installing flameshot config..."
-mkdir -p /~.config/flameshot/
-cp apps/configs/flameshot.ini ~/.config/flameshot/flameshot.ini
-
 # Mangohud config installer
 echo "Installing mangohud config..."
 mkdir -p ~/.config/MangoHud/
