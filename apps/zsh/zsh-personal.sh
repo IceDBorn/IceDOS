6b3f324a84a3ec0188c93593b428bcc18b8a405d
icedborn <github.envenomed@dralias.com>
2022-05-16 02:28:17 +0300

    Add alias to restart pipewire service

--- apps/zsh/zsh-personal.sh
+++ apps/zsh/zsh-personal.sh
@@ -8,6 +8,7 @@ alias clear-keys="sudo rm -rf ~/ local/share/keyrings/* ~/ local/share/kwalletd/
 alias max-gpu-fan-speed="sudo bash ~/.nvidia-fan-control-wayland.sh 100"
 alias pacman="sudo pacman"
 alias reboot-windows="sudo grub-set-default 0 && sudo grub-reboot 2 && sudo reboot"
+alias restart-pipewire="systemctl --user restart pipewire"
 alias ssh="TERM=xterm-256color ssh"
 alias sunshine="export DISPLAY=:0 && sunshine ~/.config/sunshine/sunshine.conf"
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
