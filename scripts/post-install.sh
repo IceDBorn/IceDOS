81f3cbec0ac83ccee3eeabe7cc276e1049758734
icedborn <github.envenomed@dralias.com>
2022-05-16 01:53:01 +0300

    Enable gnome wayland support on nvidia

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -11,3 +11,13 @@ gsettings set org.gnome.desktop.interface gtk-theme Plata-Noir-Compact
 # Change icon theme
 echo "Changing icon theme to Tela-black-dark..."
 gsettings set org.gnome.desktop.interface icon-theme Tela-black-dark
+
+# Enable nvidia wayland support
+echo "Enabling NVIDIA wayland support..."
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+gsettings set org.gnome.mutter experimental-features '["kms-modifiers"]'
+sudo sed -i '/^MODULES=/ s/btrfs/nvidia nvidia_modeset nvidia_uvm nvidia_drm &/g' /etc/mkinitcpio.conf
+sudo mkinitcpio -P
+sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ nvidia-drm.modeset=1\"/" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo pacman -Syu --needed --noconfirm xorg-xwayland libxcb egl-wayland
