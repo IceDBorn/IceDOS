512c5c6aba851b392535a50125bfa9ebfe91a75f
icedborn <github.envenomed@dralias.com>
2022-05-17 09:22:01 +0300

    Enable wayland nvidia support only if NVIDIA gpu is present

--- scripts/.post-install.sh
+++ scripts/.post-install.sh
@@ -13,14 +13,14 @@ echo "Changing icon theme to Tela-black-dark..."
 gsettings set org.gnome.desktop.interface icon-theme Tela-black-dark
 
 # Enable nvidia wayland support
-echo "Enabling NVIDIA wayland support..."
+(lspci | grep -i '.* vga .* nvidia .*' > /dev/null) && (echo "Enabling NVIDIA wayland support..."
 sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 gsettings set org.gnome.mutter experimental-features '["kms-modifiers"]'
 sudo sed -i '/^MODULES=/ s/btrfs/nvidia nvidia_modeset nvidia_uvm nvidia_drm &/g' /etc/mkinitcpio.conf
 sudo mkinitcpio -P
 sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ nvidia-drm.modeset=1\"/" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo pacman -Syu --needed --noconfirm xorg-xwayland libxcb egl-wayland
+sudo pacman -Syu --needed --noconfirm xorg-xwayland libxcb egl-wayland)
 
 # Remove script from startup
 echo "Removing script from startup"
