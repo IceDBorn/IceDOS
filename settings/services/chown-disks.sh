2be9acf4d6e35c64673b9d2fe86b28f33f388314
icedborn <github.envenomed@dralias.com>
2022-05-22 19:28:58 +0300

    Remove unused services

--- settings/services/chown-disks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-username="changethis"
-
-sudo chown "$username:$username" /mnt/Games --recursive
-sudo chown "$username:$username" /mnt/Storage --recursive
-sudo chown "$username:$username" /mnt/SSDGames --recursive
-sudo chown "$username:$username" /mnt/Windows --recursive
