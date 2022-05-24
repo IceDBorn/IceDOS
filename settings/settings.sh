9ceddcd6da253ee42f491ac469aefc5b612d024c
IceDBorn <github.envenomed@dralias.com>
2022-05-24 12:25:05 +0300

    Enable avahi service

--- settings/settings.sh
+++ settings/settings.sh
@@ -81,3 +81,7 @@ echo "Adding post install script to startup..."
 # Add noise suppression to pipewire
 echo "Adding noise suppression to pipewire..."
 ( (mkdir -p ~/.config/pipewire) && (cp /usr/share/pipewire/pipewire.conf ~/.config/pipewire/pipewire.conf) && (sed -i "/libpipewire-module-session-manager/a $(cat settings/txt-to-append/noise-suppression.txt)" ~/.config/pipewire/pipewire.conf) )
+
+# Enable avahi service
+echo "Enabling avahi service..."
+sudo systemctl enable avahi-daemon.service
