429bbee6558ec2d07c33b17e2665b48fccdbcc73
IceDBorn <github.envenomed@dralias.com>
2022-05-27 00:00:39 +0300

    Fix removal of old proton ge versions

--- scripts/.protondown.sh
+++ scripts/.protondown.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# List all proton ge versions (protonup -l), remove every character not associated with the version tag (grep -o '^\S*') and finally remove the latest version from the list (tail -n +2)
-binaries=$(tail -n +2 <<< $(protonup -l | grep -o '^\S*'))
+# Download the latest proton ge version
+yes | protonup
+
+# List all proton ge versions (protonup -l), find the latest version (grep -v), remove it from the list and remove any part not associated with the version tag (grep -o).
+binaries=$(protonup -l | grep -v "$(protonup --releases | tail -n 1)" | grep -o '^\S*')
 
 if [ -z "$binaries" ]
 then
-      echo "No proton GE versions to remove..."
+    echo "No proton GE versions to remove..."
 else
-      # Remove all proton ge versions, except the latest one
-	  while IFS= read -r line ; do yes | protonup -r "$line" && printf "\n"; done <<< "$binaries"
+        while IFS= read -r line ; do
+          # Remove "Proton-" from older versions of proton ge, because the uninstaller fails to match the folders
+          line="${line//Proton-/}"
+          # Remove all proton ge versions, except the latest one
+          yes | protonup -r "$line" && printf "\n"
+        done <<< "$binaries"
 fi
