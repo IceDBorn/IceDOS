bd5b4182fbea18ea73789dd52fb939e65ccca5a5
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-06-11 18:36:34 +0300

    Install and enable gnome extensions

--- scripts/.post-install.sh
+++ scripts/.post-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Enable nvidia wayland support
+(lspci | grep -i '.* vga .* nvidia .*' > /dev/null) && (echo "Enabling NVIDIA wayland support..."
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+gsettings set org.gnome.mutter experimental-features '["kms-modifiers"]'
+sudo mkinitcpio -P
+sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ nvidia-drm.modeset=1\"/" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo pacman -Syu --needed --noconfirm xorg-xwayland libxcb egl-wayland)
+
 # Enable fractional scaling
 echo "Enabling fractional scaling..."
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
@@ -16,14 +25,12 @@ gsettings set org.gnome.desktop.interface icon-theme Tela-black-dark
 echo "Enabling GNOME dark mode..."
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-# Enable nvidia wayland support
-(lspci | grep -i '.* vga .* nvidia .*' > /dev/null) && (echo "Enabling NVIDIA wayland support..."
-sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
-gsettings set org.gnome.mutter experimental-features '["kms-modifiers"]'
-sudo mkinitcpio -P
-sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ nvidia-drm.modeset=1\"/" /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo pacman -Syu --needed --noconfirm xorg-xwayland libxcb egl-wayland)
+# Enable extensions
+echo "Enabling gnome extensions..."
+gsettings set org.gnome.shell disable-user-extensions false
+gnome-extensions enable material-shell@papyelgringo
+gnome-extensions enable trayIconsReloaded@selfmade.pl
+gnome-extensions enable clipboard-indicator@tudmotu.com
 
 # Remove script from startup
 echo "Removing script from startup"
