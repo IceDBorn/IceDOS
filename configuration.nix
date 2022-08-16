779a8f67b1c04c01db367ee9c9e332ee48353cb9
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-16 04:14:32 +0300

    Initial gnome configuration

--- configuration.nix
+++ configuration.nix
@@ -1,130 +1,115 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    imports =
+    [
+        # Include the results of the hardware scan
+        ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    # Grub configuration
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking = {
+        # Define your hostname
+        hostname = "icedborn";
+        # Enable networking
+        networkmanager.enable = true;
+        # Disable firewall
+        firewall.enable = false;
+    };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Set your time zone.
+    time.timeZone = "Europe/Bucharest";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    i18n = {
+        defaultLocale = "en_US.utf8";
+        extraLocaleSettings = {
+            LC_MEASUREMENT = "es_ES.utf8";
+        };
+    };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Bucharest";
+    services = {
+        xserver = {
+            # Enable the X11 windowing system
+            enable = true;
+            # Enable the GNOME Desktop Environment
+            displayManager.gdm.enable = true;
+            desktopManager.gnome.enable = true;
+            # Configure keymap in X11
+            layout = "us";
+            xkbVariant = "";
+        };
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+        # Enable CUPS to print documents
+        printing.enable = true;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_ES.utf8";
-    LC_IDENTIFICATION = "es_ES.utf8";
-    LC_MEASUREMENT = "es_ES.utf8";
-    LC_MONETARY = "es_ES.utf8";
-    LC_NAME = "es_ES.utf8";
-    LC_NUMERIC = "es_ES.utf8";
-    LC_PAPER = "es_ES.utf8";
-    LC_TELEPHONE = "es_ES.utf8";
-    LC_TIME = "es_ES.utf8";
-  };
+        # Enable sound with pipewire
+        pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+        };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+        # Enable SSH
+        openssh.enable = true;
+    };
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+    sound.enable = true;
+    # Disable pulseaudio
+    hardware.pulseaudio.enable = false;
+    # Enable the RealtimeKit system service, which hands out realtime scheduling priority to user processes on demand
+    security.rtkit.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+    users = {
+        # Set default shell to zsh
+        defaultShell = pkgs.zsh;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+        # Define users below
+        users = {
+            icedborn = {
+                createHome = true;
+                home = "/home/icedborn";
+                useDefaultShell = true;
+                # Default password used for first login, change later with passwd
+                password = "1";
+                isNormalUser = true;
+                description = "IceDBorn";
+                extraGroups = [ "networkmanager" "wheel" ];
+                # Packages installed for this specific user only
+                packages = with pkgs; [
+                    firefox
+                ];
+            };
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+            work = {
+                createHome = true;
+                home = "/home/work";
+                useDefaultShell = true;
+                # Default password used for first login, change later with passwd
+                password = "1";
+                isNormalUser = true;
+                description = "Work";
+                extraGroups = [ "networkmanager" "wheel" ];
+                # Packages installed for this specific user only
+                packages = with pkgs; [
+                    firefox
+                ];
+            };
+        };
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.icedborn = {
-    isNormalUser = true;
-    description = "IceDBorn";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
+    # Packages installed system-wide
+    environment.systemPackages = with pkgs; [
+        vim
     ];
-  };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
+    # Do not change without checking the docs
+    system.stateVersion = "22.05";
 }
