ac7d277bc1ebed8d48775f638424504a283db079
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-17 01:28:18 +0300

    Remove parts already implemented into configuration file

--- settings/settings.sh
+++ settings/settings.sh
@@ -27,10 +27,6 @@ fi
 echo "Maximizing Nvidia GPU power limit..."
 (lspci | grep NVIDIA > /dev/null) && ( (echo "Maximizing Nvidia GPU power limit...") && (bash ./scripts/add-system-service.sh nv-power-limit) )
 
-# Update grub
-echo "Updating grub..."
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
 # Enable firefox wayland support
 echo "Enabling firefox wayland support..."
 cp apps/zsh/.zprofile ~/.zprofile
@@ -40,11 +36,6 @@ echo "Adding custom mozilla profile..."
 randomPath="$HOME/.mozilla/firefox/$RANDOM.privacy"
 ( (mkdir -p "$randomPath") && (cp settings/firefox/user-overrides.js "$randomPath"/user-overrides.js) && (git clone https://github.com/arkenfox/user.js.git) && (cp user.js/updater.sh "$randomPath"/updater.sh) && (sed -i "s|path-to-mozilla-updater|$randomPath|" ~/.config/zsh/zsh-personal.sh) && (yes | bash "$randomPath"/updater.sh) && (rm -rf user.js) )
 
-# Force QT applications to follow GTK theme and cursor size
-echo "XDG_CURRENT_DESKTOP=Gnome
-QT_QPA_PLATFORMTHEME=Plata-Noir-Compact
-XCURSOR_SIZE=24" | sudo tee -a /etc/environment
-
 # Add nvidia gpu fan control (wayland)
 (lspci | grep NVIDIA > /dev/null) && ( (echo "Adding nvidia gpu fan control script for wayland...") && (cp scripts/.nvidia-fan-control-wayland.sh ~/.config/zsh/scripts/.nvidia-fan-control-wayland.sh) )
 
@@ -52,16 +43,8 @@ XCURSOR_SIZE=24" | sudo tee -a /etc/environment
 echo "Adding noise suppression to pipewire..."
 ( (mkdir -p ~/.config/pipewire) && (cp /usr/share/pipewire/pipewire.conf ~/.config/pipewire/pipewire.conf) && (sed -i "/libpipewire-module-session-manager/a $(cat settings/txt-to-append/noise-suppression.txt)" ~/.config/pipewire/pipewire.conf) )
 
-# Enable avahi service
-echo "Enabling avahi service..."
-sudo systemctl enable avahi-daemon.service
-
-# Setup sunshine setcap
-echo "Setting sunshine setcap..."
-sudo setcap cap_sys_admin+p /usr/bin/sunshine
-
 # Add proton remove script to zsh scripts
-pip list | grep -F protonup-ng && (echo "Adding proton ge version remover script...") && (cp scripts/.protondown.sh ~/.config/zsh/scripts/.protondown.sh)
+(cp scripts/.protondown.sh ~/.config/zsh/scripts/.protondown.sh)
 
 # Add post install script to startup
 echo "Adding post install script to startup..."
