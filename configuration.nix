f74cd4833e05c2e4adb22393f3555343587166fe
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-16 17:12:12 +0300

    Add more main user packages

--- configuration.nix
+++ configuration.nix
@@ -92,11 +92,18 @@
                 extraGroups = [ "networkmanager" "wheel" ];
                 # Packages installed for this specific user only
                 packages = with pkgs; [
-                    scanmem # Cheat engine for linux
+                    bottles # Wine prefix manager
+                    duckstation # PS1 Emulator
                     gamemode # Optimizations for gaming
                     godot # Game engine
+                    heroic # Epic Games Launcher for Linux
+                    mangohud # A metric overlay
                     pcsx2 # PS2 Emulator
                     ppsspp # PSP Emulator
+                    protontricks # Winetricks for proton prefixes
+                    rpcs3 # PS3 Emulator
+                    ryujinx # Switch Emulator
+                    scanmem # Cheat engine for linux
                     steam # Gaming platform
                 ];
             };
