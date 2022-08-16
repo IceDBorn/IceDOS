74ccbd252f4c66c27b67b2a78e0d1eb10606d1a7
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-16 17:11:48 +0300

    Add more system-wide packages

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
         # Use Zen kernel
         kernelPackages = pkgs.linuxPackages_zen;
         # Virtual camera for OBS
-        kernelModules = [ "v4l2loopback" ];
+        kernelModules = [ "v4l2loopback" "xpadneo" ];
     };
 
     networking = {
@@ -112,7 +112,7 @@
                 extraGroups = [ "networkmanager" "wheel" ];
                 # Packages installed for this specific user only
                 packages = with pkgs; [
-                    firefox
+                    #Add work specific packages here
                 ];
             };
         };
@@ -131,19 +131,39 @@
         gimp # Image editor
         git # Distributed version control system
         gnome.gnome-tweaks # Tweaks missing from pure Gnome
+        gnomeExtensions.application-volume-mixer # Application volume mixer on the gnome control center
+        gnomeExtensions.bluetooth-quick-connect # Show bluetooth devices on the gnome control center
+        gnomeExtensions.clipboard-indicator # Clipboard indicator for gnome
+        gnomeExtensions.color-picker # Color picker for gnome
+        gnomeExtensions.coverflow-alt-tab # Makes alt tab on gnome cooler
+        gnomeExtensions.gamemode # Status indicator for gamemode on gnome
+        gnomeExtensions.gsconnect # KDE Connect implementation for gnome
+        gnomeExtensions.material-shell # Tiling WM for gnome
+        gnomeExtensions.sound-output-device-chooser # Sound devices choose on the gnome control center
+        gnomeExtensions.tray-icons-reloaded # Tray icons for gnome
         helvum # Pipewire patchbay
+        jetbrains.webstorm # All purpose IDE
+        mullvad-vpn # VPN Client
         ntfs3g # Support NTFS drives
         obs-studio # Recording/Livestream
+        onlyoffice-bin # Microsoft Office alternative for Linux
         os-prober # Detect all operating systems
         pitivi # Video editor
+        plata-theme # Gnome theme
         ranger # Terminal file manager
+        rnnoise-plugin # A real-time noise suppression plugin
         signal-desktop # Encrypted messaging platform
+        sublime4 # Text editor
         syncthing # Local file sync
+        tela-icon-theme # Icon theme
         tree # Display folder content at a tree format
         unrar # Support opening rar files
+        usbimager # ISO Burner
         wget # Terminal downloader
         wine # Compatibility layer capable of running Windows applications
         winetricks # Wine prefix settings manager
+        woeusb # Windows ISO Burner
+        zenstates # Ryzen CPU controller
     ];
 
     # Do not change without checking the docs
