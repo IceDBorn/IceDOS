5b6774a909a0ec32369c4058ee87d48ad324b0a4
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-17 06:00:31 +0300

    Add work user to home manager

--- configuration.nix
+++ configuration.nix
@@ -9,61 +9,98 @@ in
         (import "${home-manager}/nixos")
     ];
 
-    home-manager.users.icedborn = {
-        programs = {
-            git = {
-                enable = true;
-                # Git config
-                userName  = "IceDBorn";
-                userEmail = "github.envenomed@dralias.com";
-            };
-
-            alacritty = {
-                enable = true;
-                # Alacritty config
-                settings = {
-                    window = {
-                        decorations = "none";
-                        opacity = 0.8;
+    home-manager = {
+        users = {
+            icedborn = {
+                programs = {
+                    git = {
+                        enable = true;
+                        # Git config
+                        userName  = "IceDBorn";
+                        userEmail = "github.envenomed@dralias.com";
                     };
 
-                    cursor.style = {
-                        shape = "Underline";
-                        blinking = "Always";
+                    alacritty = {
+                        enable = true;
+                        # Alacritty config
+                        settings = {
+                            window = {
+                                decorations = "none";
+                                opacity = 0.8;
+                            };
+
+                            cursor.style = {
+                                shape = "Underline";
+                                blinking = "Always";
+                            };
+                        };
+                    };
+
+                    mangohud = {
+                        enable = true;
+                        # MangoHud is started on any application that supports it
+                        enableSessionWide = true;
+                        # Mangohud config
+                        settings = {
+                            background_alpha = 0;
+                            cpu_color = "FFFFFF";
+                            engine_color = "FFFFFF";
+                            font_size = 20;
+                            fps_limit = "144+60+0";
+                            frame_timing = 0;
+                            gl_vsync = 0;
+                            gpu_color = "FFFFFF";
+                            offset_x = 50;
+                            position = "top-right";
+                            toggle_fps_limit = "F1";
+                            vsync= 1;
+                            cpu_temp = "";
+                            fps = "";
+                            gamemode = "";
+                            gpu_temp = "";
+                            no_small_font = "";
+                        };
+                    };
+
+                    zsh = {
+                        enable = true;
+                        # Enable firefox wayland
+                        profileExtra = "export MOZ_ENABLE_WAYLAND=1";
                     };
                 };
             };
 
-            mangohud = {
-                enable = true;
-                # MangoHud is started on any application that supports it
-                enableSessionWide = true;
-                # Mangohud config
-                settings = {
-                    background_alpha = 0;
-                    cpu_color = "FFFFFF";
-                    engine_color = "FFFFFF";
-                    font_size = 20;
-                    fps_limit = "144+60+0";
-                    frame_timing = 0;
-                    gl_vsync = 0;
-                    gpu_color = "FFFFFF";
-                    offset_x = 50;
-                    position = "top-right";
-                    toggle_fps_limit = "F1";
-                    vsync= 1;
-                    cpu_temp = "";
-                    fps = "";
-                    gamemode = "";
-                    gpu_temp = "";
-                    no_small_font = "";
-                };
-            };
+            work = {
+                programs = {
+                    git = {
+                        enable = true;
+                        # Git config
+                        userName  = "IceDBorn";
+                        userEmail = "github.envenomed@dralias.com";
+                    };
 
-            zsh = {
-                enable = true;
-                # Enable firefox wayland
-                profileExtra = "export MOZ_ENABLE_WAYLAND=1";
+                    alacritty = {
+                        enable = true;
+                        # Alacritty config
+                        settings = {
+                            window = {
+                                decorations = "none";
+                                opacity = 0.8;
+                            };
+
+                            cursor.style = {
+                                shape = "Underline";
+                                blinking = "Always";
+                            };
+                        };
+                    };
+
+                    zsh = {
+                        enable = true;
+                        # Enable firefox wayland
+                        profileExtra = "export MOZ_ENABLE_WAYLAND=1";
+                    };
+                };
             };
         };
     };
