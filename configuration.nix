5d1811596010c5c705004dbbcb5372fb52b155fb
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-17 05:22:12 +0300

    Install home manager

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
 
+let
+    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+    imports = [
+        (import "${home-manager}/nixos")
+    ];
+
+    home-manager.users.icedborn = {
+        /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
+    };
+}
+
 {
     imports =
     [
