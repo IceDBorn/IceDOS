d4bd3d9fd186e4309ac90dd1b99d1c13465abe9e
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-17 15:08:08 +0300

    Remove script parts already implemented into configuration.nix

--- apps/install-app-configs.sh
+++ apps/install-app-configs.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Mangohud config installer
-echo "Installing mangohud config..."
-( (mkdir -p ~/.config/MangoHud/) && (cp apps/configs/MangoHud.conf ~/.config/MangoHud/MangoHud.conf) )
-
-# Sunshine config installer
-echo "Installing sunshine config..."
-( (mkdir -p ~/.config/sunshine/) && (cp apps/configs/sunshine.conf ~/.config/sunshine/sunshine.conf) )
-
-# Alacritty config installer
-echo "Installing alacritty config..."
-( (mkdir -p ~/.config/alacritty/) && (cp apps/configs/alacritty.yml ~/.config/alacritty/alacritty.yml) )
-
 # Alacritty multiple terminals desktop file installer
 echo "Installing alacritty multiple terminals desktop file..."
 ( (mkdir -p ~/.local/share/applications/) && (cp apps/startup/startup-terminals.desktop ~/.local/share/applications/startup-terminals.desktop) )
