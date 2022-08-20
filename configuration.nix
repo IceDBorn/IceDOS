5775fe1441b7fe158aafc102375fc6292fc3a382
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2022-08-20 15:11:43 +0300

    Switch to stable home manager

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 
 # Install home manager
 let
-    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
 in
 {
     imports = [
