376ae8e4644685460bcc0de21f64875a827167d4
IceDBorn <github.envenomed@dralias.com>
2022-08-21 06:08:25 +0300

    Add nix user repository

--- configuration.nix
+++ configuration.nix
@@ -529,8 +529,17 @@ in
         };
     };
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs.config = {
+         # Allow proprietary packages
+        allowUnfree = true;
+
+        # Install NUR
+        packageOverrides = pkgs: {
+            nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+                inherit pkgs;
+            };
+        };
+    };
 
     # Packages installed system-wide
     environment.systemPackages = with pkgs; [
