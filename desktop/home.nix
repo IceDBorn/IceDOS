f42906e76db90826694dc28c2ac913b7b9624c53
IceDBorn <github.envenomed@dralias.com>
2022-08-23 17:51:15 +0300

    Force electron apps to use wayland

--- desktop/home.nix
+++ desktop/home.nix
@@ -98,6 +98,40 @@
                 comment = "A fast, cross-platform, OpenGL terminal emulator";
             };
 
+            # Force vscodium to use wayland
+            xdg.desktopEntries.codium = {
+                type = "Application";
+                exec = "codium --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
+                icon = "code";
+                terminal = false;
+                categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+                name = "VSCodium";
+                genericName = "Text Editor";
+                comment = "Code Editing. Redefined.";
+                actions.new-empty-window = {
+                    "exec" = "codium --new-window %F";
+                    "icon" = "code";
+                    "name" = "New Empty Window";
+                };
+            };
+
+            # Force signal to use wayland
+            xdg.desktopEntries.signal-desktop = {
+                type = "Application";
+                exec = "signal-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+                icon = "signal-desktop";
+                terminal = false;
+                categories = [ "Network" "InstantMessaging" "Chat" ];
+                name = "Signal";
+                genericName = "Text Editor";
+                comment = "Private messaging from your desktop";
+                actions.new-empty-window = {
+                    "exec" = "codium --new-window %F";
+                    "icon" = "code";
+                    "name" = "New Empty Window";
+                };
+            };
+
             home.file = {
                 # Add zsh theme to zsh directory
                 ".config/zsh/zsh-theme.sh" = {
@@ -231,6 +265,40 @@
                 name = "Startup Terminals";
                 genericName = "Terminal";
                 comment = "A fast, cross-platform, OpenGL terminal emulator";
+            };
+
+            # Force vscodium to use wayland
+            xdg.desktopEntries.codium = {
+                type = "Application";
+                exec = "codium --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
+                icon = "code";
+                terminal = false;
+                categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+                name = "VSCodium";
+                genericName = "Text Editor";
+                comment = "Code Editing. Redefined.";
+                actions.new-empty-window = {
+                    "exec" = "codium --new-window %F";
+                    "icon" = "code";
+                    "name" = "New Empty Window";
+                };
+            };
+
+            # Force signal to use wayland
+            xdg.desktopEntries.signal-desktop = {
+                type = "Application";
+                exec = "signal-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+                icon = "signal-desktop";
+                terminal = false;
+                categories = [ "Network" "InstantMessaging" "Chat" ];
+                name = "Signal";
+                genericName = "Text Editor";
+                comment = "Private messaging from your desktop";
+                actions.new-empty-window = {
+                    "exec" = "codium --new-window %F";
+                    "icon" = "code";
+                    "name" = "New Empty Window";
+                };
             };
 
             home.file = {
