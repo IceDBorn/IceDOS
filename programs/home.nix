07e70d646bf3309869d3275a0fe402986a45f843
IceDBorn <github.envenomed@dralias.com>
2022-08-23 22:46:38 +0300

    Revert to wayland, disable decorations for kitty

--- programs/home.nix
+++ programs/home.nix
@@ -20,10 +20,10 @@ in
                         cursor_shape = "underline";
                         cursor_underline_thickness = "1.0";
                         background_opacity = "0.8";
-                        linux_display_server = "x11";
                         update_check_interval = "0";
                         confirm_os_window_close = "0";
                         enable_audio_bell = "no";
+                        hide_window_decorations = "yes";
                     };
                 };
 
@@ -88,10 +88,10 @@ in
                         cursor_shape = "underline";
                         cursor_underline_thickness = "1.0";
                         background_opacity = "0.8";
-                        linux_display_server = "x11";
                         update_check_interval = "0";
                         confirm_os_window_close = "0";
                         enable_audio_bell = "no";
+                        hide_window_decorations = "yes";
                     };
                 };
 
