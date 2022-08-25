98b35f12b8b9b8a043e3d393e83a6a84b91b17cd
IceDBorn <github.envenomed@dralias.com>
2022-08-25 15:46:40 +0300

    Disable switch to application shortcuts

--- desktop/home.nix
+++ desktop/home.nix
@@ -90,6 +90,19 @@
                     ];
                 };
 
+                # Disable switch to application shortcuts
+                "org/gnome/shell/keybindings" = {
+                    switch-to-application-1 = [];
+                    switch-to-application-2 = [];
+                    switch-to-application-3 = [];
+                    switch-to-application-4 = [];
+                    switch-to-application-5 = [];
+                    switch-to-application-6 = [];
+                    switch-to-application-7 = [];
+                    switch-to-application-8 = [];
+                    switch-to-application-9 = [];
+                };
+
                 # Limit app switcher to current workspace
                 "org/gnome/shell/app-switcher" = {
                     current-workspace-only = true;
--- programs/firefox-pwas.nix
+++ programs/firefox-pwas.nix
@@ -14,6 +14,14 @@ let
 
     cargoHash = "sha256-xsPwuOxg3AR+wvXwTBSakImDPH4VKFG2R3UcScUI0Eo=";
 
+    # patchPhase = ''
+    #     sed -i "s/version = \"0.0.0\"/version = \"${version}\"/g" Cargo.toml
+    #     sed -i "s/DISTRIBUTION_VERSION = '0.0.0'/DISTRIBUTION_VERSION = '${version}'/g" userchrome/profile/chrome/pwa/chrome.jsm
+    # '';
+
+    FFPWA_EXECUTABLES = "{pkgs.firefox-pwas}/bin/firefoxpwa";
+    FFPWA_SYSDATA = "{pkgs.firefox-pwas}/native/userchrome";
+
     meta = with lib; {
         description = "A tool to install, manage and use Progressive Web Apps (PWAs) in Mozilla Firefox.";
         homepage = "https://github.com/filips123/PWAsForFirefox";
