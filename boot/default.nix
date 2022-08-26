a934531e3ec3cc31f315777393f90807e2ddab6c
IceDBorn <github.envenomed@dralias.com>
2022-08-26 17:22:00 +0300

    Set boot timeout to 1 second

--- boot/default.nix
+++ boot/default.nix
@@ -3,6 +3,7 @@
 {
     boot = {
         loader = {
+            # Enable EFI boot
             efi = {
                 canTouchEfiVariables = true;
                 efiSysMountPoint = "/boot/efi";
@@ -13,6 +14,9 @@
                 # Keep the last 10 nixOS configurations
                 configurationLimit = 10;
             };
+
+            # Boot to default entry after 1 second
+            timeout = 1;
         };
         # Use Zen kernel
         kernelPackages = pkgs.linuxPackages_zen;
