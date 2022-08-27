e2b51faa15e66a261ddd777f698ac68970b4b43a
IceDBorn <github.envenomed@dralias.com>
2022-08-27 05:38:39 +0300

    Fix install, update script

--- install.sh
+++ install.sh
@@ -17,14 +17,15 @@ read -r -p "Have you customized the setup to your needs? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
   # Add configuration files to the appropriate path
-  sudo cp configuration.nix /etc/nixos
+  sudo cp -r boot /etc/nixos
   sudo cp -r configs /etc/nixos
-  sudo cp -r scripts /etc/nixos
-  sudo cp -r programs /etc/nixos
   sudo cp -r desktop /etc/nixos
   sudo cp -r hardware /etc/nixos
+  sudo cp -r programs /etc/nixos
+  sudo cp -r scripts /etc/nixos
+  sudo cp -r startup /etc/nixos
   sudo cp -r users /etc/nixos
-  sudo cp -r boot /etc/nixos
+  sudo cp configuration.nix /etc/nixos
 
   # Build the configuration
   sudo nixos-rebuild switch || exit
--- update.sh
+++ update.sh
@@ -4,14 +4,15 @@
 cd "$(cd "$(dirname "$0")" && pwd)" || exit
 
 # Add configuration files to the appropriate path
-sudo cp configuration.nix /etc/nixos
+sudo cp -r boot /etc/nixos
 sudo cp -r configs /etc/nixos
-sudo cp -r scripts /etc/nixos
-sudo cp -r programs /etc/nixos
 sudo cp -r desktop /etc/nixos
 sudo cp -r hardware /etc/nixos
+sudo cp -r programs /etc/nixos
+sudo cp -r scripts /etc/nixos
+sudo cp -r startup /etc/nixos
 sudo cp -r users /etc/nixos
-sudo cp -r boot /etc/nixos
+sudo cp configuration.nix /etc/nixos
 
 # Build the configuration
 sudo nixos-rebuild switch || exit
