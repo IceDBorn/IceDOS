8e54721694fca12747c106bcb81517a0c94ef830
IceDBorn <github.envenomed@dralias.com>
2022-08-29 02:54:55 +0300

    Fix mullvad on startup

--- desktop/home.nix
+++ desktop/home.nix
@@ -283,7 +283,17 @@
 
                 # Add mullvad vpn to startup
                 ".config/autostart/mullvad-vpn.desktop" = {
-                    source = ../startup/mullvad-vpn.desktop;
+                    text = ''
+                        [Desktop Entry]
+                        Name=Mullvad VPN
+                        Exec=${pkgs.mullvad-vpn}/bin/mullvad-vpn --enable-features=UseOzonePlatform --ozone-platform=wayland
+                        Terminal=false
+                        Type=Application
+                        Icon=mullvad-vpn
+                        StartupWMClass=Mullvad VPN
+                        Comment=Mullvad VPN client
+                        Categories=Network;
+                    '';
                     recursive = true;
                 };
 
