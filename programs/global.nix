d679ac0a701e90c2bb9e8e669e4cc022002821cf
IceDBorn <github.envenomed@dralias.com>
2022-10-25 20:21:21 +0300

    Add alias to counter steam link bugs

--- programs/global.nix
+++ programs/global.nix
@@ -59,6 +59,7 @@
                 nvidia-max-fan-speed="sudo bash ~/.config/zsh/nvidia-fan-control-wayland.sh 100"; # Maximize nvidia fan speed on wayland
                 restart-pipewire="systemctl --user restart pipewire"; # Restart pipewire
                 ssh="TERM=xterm-256color ssh"; # SSH with colors
+                steam-link="gamescope -H 1080 -b -- steam"; # Launch steam inside of a gamescope instance
                 update="(sudo nixos-rebuild switch --upgrade) ; (flatpak update) ; (yes | protonup) ; (yes | ~/.mozilla/firefox/privacy/updater.sh)"; # Update everything
                 vpn-off="mullvad disconnect"; # Disconnect from VPN
                 vpn-on="mullvad connect"; # Connect to VPN
