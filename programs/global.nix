ff49f062a189a5763367e39fa47ebc225cb067b5
IceDBorn <github.envenomed@dralias.com>
2022-11-20 02:47:58 +0200

    Remove hyprlog and use 'hyprctl clients' for watching windows

--- programs/global.nix
+++ programs/global.nix
@@ -61,7 +61,6 @@
 				clear-keys="sudo rm -rf ~/ local/share/keyrings/* ~/ local/share/kwalletd/*"; # Clear system keys
 				clear-proton-ge="bash ~/.config/zsh/protondown.sh"; # Download the latest proton ge version and delete the older ones
 				desktop-files-list="ls -l /run/current-system/sw/share/applications"; # Show desktop files location
-				hyprlog="watch -n 0.1 'cat /tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/hyprland.log | grep -v \"arranged\" | tail -n 40'"; # Watch hyprland logs
 				list-packages="nix-store --query --requisites /run/current-system | cut -d- -f2- | sort | uniq"; # List installed nix packages
 				restart-pipewire="systemctl --user restart pipewire"; # Restart pipewire
 				ssh="TERM=xterm-256color ssh"; # SSH with colors
