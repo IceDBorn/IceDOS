57d2f5b970a8da2476b994e36210662f44e21263
IceDBorn <github.envenomed@dralias.com>
2022-12-10 00:00:37 +0200

    Override waybar attrs instead of self-bulding it

--- desktop/wms/hyprland/default.nix
+++ desktop/wms/hyprland/default.nix
@@ -12,7 +12,12 @@
 
 	environment = {
 		systemPackages = with pkgs; [
-			(callPackage ../../../programs/self-built/waybar.nix {}) # Status bar
+			(waybar.overrideAttrs (oldAttrs: {
+				mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+				postPatch = ''
+					sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
+				'';
+			})) # Status bar
 			baobab # Disk usage analyser
 			blueberry # Bluetooth manager
 			clipman # Clipboard manager for wayland
