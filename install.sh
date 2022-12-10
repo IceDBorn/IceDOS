5b070f32881c9690168e8fb0fd75793967ed4781
IceDBorn <github.envenomed@dralias.com>
2022-12-10 09:23:14 +0200

    Update install script for new file structure

--- install.sh
+++ install.sh
@@ -17,15 +17,12 @@ read -r -p "Have you customized the setup to your needs? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
 	# Add configuration files to the appropriate path
-	sudo cp -r boot /etc/nixos
-	sudo cp -r configs /etc/nixos
-	sudo cp -r desktop /etc/nixos
+	sudo cp -r bootloader /etc/nixos
 	sudo cp -r hardware /etc/nixos
-	sudo cp -r programs /etc/nixos
-	sudo cp -r scripts /etc/nixos
-	sudo cp -r startup /etc/nixos
-	sudo cp -r users /etc/nixos
+	sudo cp -r system /etc/nixos
 	sudo cp configuration.nix /etc/nixos
+	sudo cp flake.nix /etc/nixos
+	sudo cp flake.lock /etc/nixos
 
 	# Remove potentially generated firefox profiles before installing the nix configuration
 	sudo rm -rf /home/$username/.mozilla/firefox/profiles.ini
