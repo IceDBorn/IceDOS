2e67605da3582e1f8425d534bb9f6843da746b48
Stefanos Papanatsios <github.ekta@aleeas.com>
2022-12-13 00:33:08 +0200

    Remove/replace gnome extensions and ignore .idea folder (#16)
    * Remove sound output device chooser from gnome
    
    * Ignore .idea folder
    
    * Remove tray icons reloaded and volume mixer - Add appindicator
    
    * Add quick settings tweaks gnome extension
    
    * Fix typo
    
    * Remove tray icons reloaded and sound output device chooser from home manager

--- system/desktop/gnome/default.nix
+++ system/desktop/gnome/default.nix
@@ -14,7 +14,7 @@
 	environment.systemPackages = with pkgs; [
 		gnome.dconf-editor # Edit gnome's dconf
 		gnome.gnome-tweaks # Tweaks missing from pure Gnome
-		gnomeExtensions.application-volume-mixer # Application volume mixer on the gnome control center
+		gnomeExtensions.appindicator # Tray icons for gnome
 		gnomeExtensions.bluetooth-quick-connect # Show bluetooth devices on the gnome control center
 		gnomeExtensions.caffeine # Disable auto suspend and screen blank
 		gnomeExtensions.clipboard-indicator # Clipboard indicator for gnome
@@ -23,9 +23,8 @@
 		gnomeExtensions.gamemode # Status indicator for gamemode on gnome
 		gnomeExtensions.gsconnect # KDE Connect implementation for gnome
 		gnomeExtensions.pop-shell # Tiling WM extension
+		gnomeExtensions.quick-settings-tweaker # Quick settings tweaker for the gnome control center
 		gnomeExtensions.smart-auto-move # Remember window state througout reboots and restore it
-		gnomeExtensions.sound-output-device-chooser # Sound devices choose on the gnome control center
-		gnomeExtensions.tray-icons-reloaded # Tray icons for gnome
 		gnome-extension-manager # Gnome extensions manager and downloader
 	];
 
