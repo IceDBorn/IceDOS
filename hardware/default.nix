3e7f1c4eab0915084eff6cfd309fba8ceefadaf2
IceDBorn <github.envenomed@dralias.com>
2023-01-12 06:18:31 +0200

    Fix nvidia-offload being installed without an nvidia gpu

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,5 +1,14 @@
-{lib, config, ...}:
+{lib, config, pkgs, ...}:
 
+let
+	nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+		export __NV_PRIME_RENDER_OFFLOAD=1
+		export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+		export __GLX_VENDOR_LIBRARY_NAME=nvidia
+		export __VK_LAYER_NV_optimus=NVIDIA_only
+		exec "$@"
+	'';
+in
 {
 	hardware = {
 		opengl = {
@@ -17,6 +26,8 @@
 		bluetooth.enable = true;
 		uinput.enable = true; # Enable uinput support
 	};
+
+	environment.systemPackages = lib.mkIf (config.laptop.enable && config.nvidia.enable) [ nvidia-offload ]; # Use nvidia-offload to launch programs using the nvidia GPU
 
 	# Set memory limits
 	security.pam.loginLimits =
--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -1,17 +1,8 @@
 { pkgs, lib, config, ...}:
 
-let
-	nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-		export __NV_PRIME_RENDER_OFFLOAD=1
-		export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-		export __GLX_VENDOR_LIBRARY_NAME=nvidia
-		export __VK_LAYER_NV_optimus=NVIDIA_only
-		exec "$@"
-	'';
-in
 lib.mkIf config.laptop.enable {
 	services.auto-cpufreq.enable = true;
-	environment.systemPackages = [ pkgs.brightnessctl nvidia-offload ];
+	environment.systemPackages = [ pkgs.brightnessctl ];
 
 	hardware.nvidia.prime = lib.mkIf config.nvidia.enable {
 		offload.enable = true;
