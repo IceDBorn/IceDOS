8b8a3634dc12a152b7efa4230290a8e837d5dc35
IceDBorn <github.envenomed@dralias.com>
2023-02-21 08:51:19 +0200

    Fix disabling users

--- system/users/main.nix
+++ system/users/main.nix
@@ -1,14 +1,14 @@
 { config, lib, ... }:
 
-{
+lib.mkIf config.main.user.enable {
 	users.users.${config.main.user.username} = {
 		createHome = true;
 		home = "/home/${config.main.user.username}";
 		useDefaultShell = true;
 		password = "1"; # Default password used for first login, change later with passwd
 		isNormalUser = true;
-		description = lib.mkIf config.main.user.enable "${config.main.user.description}";
-		extraGroups = lib.mkIf config.main.user.enable [ "networkmanager" "wheel" "kvm" "docker" ];
+		description = "${config.main.user.description}";
+		extraGroups = [ "networkmanager" "wheel" "kvm" "docker" ];
 	};
 
 	home-manager.users.${config.main.user.username}.home = {
