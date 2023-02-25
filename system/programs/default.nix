7867c228d81708b901e0a2c73188f1bfa0d0c68a
IceDBorn <github.envenomed@dralias.com>
2023-02-25 21:57:12 +0200

    Add local server to substituters

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -16,7 +16,7 @@
 			auto-optimise-store = true; # Use hard links to save space (slows down package manager)
 			experimental-features = [ "nix-command" "flakes" ]; # Enable flakes
 			# Hyprland cachix
-			substituters = ["https://hyprland.cachix.org"];
+			substituters = [ "http://192.168.1.2:8080" "https://hyprland.cachix.org" ];
 			trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 		};
 
