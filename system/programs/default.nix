1f4060619089901b0f5a7ccf9baf234cf81cd3d4
IceDBorn <github.envenomed@dralias.com>
2023-03-07 01:05:51 +0200

    Use hyprland cachix before local server

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -15,9 +15,8 @@
 		settings = {
 			auto-optimise-store = true; # Use hard links to save space (slows down package manager)
 			experimental-features = [ "nix-command" "flakes" ]; # Enable flakes
-			# Hyprland cachix
-			substituters = [ "http://192.168.1.2:8080" "https://hyprland.cachix.org" ];
-			trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+			substituters = [ "https://hyprland.cachix.org" "http://192.168.1.2:8080" ]; # Hyprland cachix
+			trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
 		};
 
 		# Automatic garbage collection
