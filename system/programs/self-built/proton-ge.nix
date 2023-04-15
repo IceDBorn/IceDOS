c5938a692a8cfe0d34515c7205c22246d01dcddc
IceDBorn <github.envenomed@dralias.com>
2023-04-16 02:08:39 +0300

    proton-ge: 7-54 -> 7-55

--- system/programs/self-built/proton-ge.nix
+++ system/programs/self-built/proton-ge.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
 	name = "proton-ge";
-	version = "GE-Proton7-54";
+	version = "GE-Proton7-55";
 
 	src = builtins.fetchTarball {
 		url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-		sha256 = "1iy14s1d48wxnnmw45jh5w2yadkrvwip8k91xljwg066aprb00vi";
+		sha256 = "0szrza88ic0rx6y90y1s655faxfz7lq24315zw0xl107gvszw8p8";
 	};
 
 	preferLocalBuild = true;
