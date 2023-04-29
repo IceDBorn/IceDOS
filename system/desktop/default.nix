57aba620acf99d2e04b67b33562cbc45247b06b1
IceDBorn <github.envenomed@dralias.com>
2023-04-29 11:05:47 +0300

    Remove qt theme setup because it does not work

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -64,10 +64,6 @@
 	security.sudo.extraConfig = "Defaults pwfeedback"; # Show asterisks when typing sudo password
 
 	environment = {
-		sessionVariables = {
-			QT_QPA_PLATFORMTHEME= "gnome"; # Use gtk theme for qt apps
-		};
-
 		# Packages to install for all window manager/desktop environments
 		systemPackages = with pkgs; [
 			bibata-cursors # Material cursors
@@ -76,7 +72,6 @@
 			gnome.gnome-boxes # VM manager
 			gthumb # Image viewer
 			pitivi # Video editor
-			qgnomeplatform # Use GTK theme for QT apps
 			tela-icon-theme # Icon theme
 		];
 	};
