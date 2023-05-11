8ecdf5e841eab897a8f1c9b0deded7b215e36cb6
IceDBorn <github.envenomed@dralias.com>
2023-05-11 07:58:40 +0300

    Add proton-ge sha256

--- system/programs/self-built/proton-ge.nix
+++ system/programs/self-built/proton-ge.nix
@@ -6,7 +6,7 @@ stdenvNoCC.mkDerivation rec {
 
 	src = builtins.fetchTarball {
 		url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-		sha256 = "";
+		sha256 = "0qzx52gz5y18mcvlxb26scr7x4jk4zshrziykhy069099w61ldjx";
 	};
 
 	preferLocalBuild = true;
