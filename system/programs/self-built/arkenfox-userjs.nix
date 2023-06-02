c04035cd52e2e68a15dcba0e3b41f4a98822bc9a
IceDBorn <github.envenomed@dralias.com>
2023-06-02 22:51:45 +0300

    Fix privacy firefox config

---
+++ system/programs/self-built/arkenfox-userjs.nix
@@ -0,0 +1,21 @@
+{ stdenvNoCC, fetchFromGitHub, python3, ... }:
+
+stdenvNoCC.mkDerivation rec {
+	name = "arkenfox-userjs";
+	version = "112.0";
+
+	src = fetchFromGitHub {
+		owner = "arkenfox";
+		repo = "user.js";
+		rev = version;
+		sha256 = "k4PF8FWN6U+//UmZX4UxzBWbfAgEwQznLVsaFV/fVKo=";
+	};
+
+	preferLocalBuild = true;
+
+	installPhase = ''
+		cat ${../../configs/firefox/user-overrides.js} >> user.js
+		mkdir -p $out
+		cp ./user.js $out/user.js
+	'';
+}
