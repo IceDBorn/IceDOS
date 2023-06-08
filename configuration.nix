648d851e2836328558c745c2d9b3b60735a7d822
IceDBorn <github.envenomed@dralias.com>
2023-06-09 01:40:59 +0300

    Bump nix version

--- configuration.nix
+++ configuration.nix
@@ -22,5 +22,5 @@
 		./system/users
 	];
 
-	config.system.stateVersion = "22.05"; # Do not change without checking the docs
+	config.system.stateVersion = "23.05"; # Do not change without checking the docs
 }
