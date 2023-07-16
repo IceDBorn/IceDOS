ab1d8dd04bc44812c22741c4b970fb183fd7f023
CrazyStevenz <github.ekta@aleeas.com>
2023-07-17 01:37:31 +0300

    Remove unused startup apps from gnome

--- system/desktop/gnome/startup/work.nix
+++ system/desktop/gnome/startup/work.nix
@@ -2,20 +2,6 @@
 
 lib.mkIf config.work.user.enable {
 	home-manager.users.${config.work.user.username}.home.file = lib.mkIf (config.desktop-environment.gnome.enable && config.desktop-environment.gnome.configuration.startup-items.enable) {
-		# Add discord-screenaudio to startup
-		".config/autostart/discord-screenaudio.desktop" = {
-			text = ''
-				[Desktop Entry]
-				Exec=apx --aur run discord-screenaudio
-				Icon=discord-screenaudio
-				Name=discord-screenaudio
-				StartupWMClass=discord-screenaudio
-				Terminal=false
-				Type=Application
-			'';
-			recursive = true;
-		};
-
 		# Add signal to startup
 		".config/autostart/slack.desktop" = {
 			text = ''
@@ -24,20 +10,6 @@ lib.mkIf config.work.user.enable {
 				Icon=slack
 				Name=Slack
 				StartupWMClass=slack
-				Terminal=false
-				Type=Application
-			'';
-			recursive = true;
-		};
-
-		# Add terminator to startup
-		".config/autostart/terminator.desktop" = {
-			text = ''
-				[Desktop Entry]
-				Exec=terminator
-				Icon=terminator
-				Name=Terminator
-				StartupWMClass=terminator
 				Terminal=false
 				Type=Application
 			'';
