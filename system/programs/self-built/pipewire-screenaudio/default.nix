8a3c7b4a2661cfbf26b44ac53e50c29a02b46ebf
IceDBorn <github.envenomed@dralias.com>
2023-07-19 19:25:14 +0300

    pipewire-screenaudio: 0.1.0 -> 0.2.0

--- system/programs/self-built/pipewire-screenaudio/default.nix
+++ system/programs/self-built/pipewire-screenaudio/default.nix
@@ -1,52 +1,25 @@
-{ lib, stdenv, fetchFromGitHub, pipewire, cmake, git, tl-expected, jq, hexdump, gawk, ... }:
+{ lib, stdenv, fetchFromGitHub, pipewire, jq, hexdump, gawk, psmisc, ... }:
 
 stdenv.mkDerivation rec {
 	pname = "pipewire-screenaudio";
-  	version = "0.1.0";
+  	version = "0.2.0";
 
 	src = fetchFromGitHub {
 		owner = "IceDBorn";
 		repo = "pipewire-screenaudio";
 		rev = version;
-		sha256 = "Sc2Dk/idHpGrGyaj98148bo9xeLQ1ITbRlTN+J7nYg4=";
-		fetchSubmodules = true;
+		sha256 = "IfPW0qmIUMIuevMLolYyKpYMBiiBG1OJA7/Wtxp+EzM=";
   	};
 
-	patches = [ ./create-rohrkabel-cmake.patch ];
-
-	NIX_CFLAGS_COMPILE = [
-		"-I${pipewire.dev}/include/pipewire-0.3"
-		"-I${pipewire.dev}/include/spa-0.2"
-		"-Wno-pedantic" # Fails without flag
-	];
-
-	dontUseCmakeConfigure = true;
-
-	nativeBuildInputs = [ cmake git ];
-
-	buildInputs = [ pipewire tl-expected jq hexdump gawk ];
-
-	libPath = lib.makeLibraryPath [ pipewire ];
-
-	buildPhase = ''
-		rootPath=`pwd`
-		cd native
-
-		(
-			cd ./pipewire-screenaudio/rohrkabel/
-			git apply $rootPath/rohrkabel-cmake.patch
-		)
-
-		bash build.sh
-	'';
+  buildInputs = [ pipewire jq hexdump gawk psmisc ];
 
 	installPhase = ''
 		mkdir -p $out/lib/out
-		install -Dm755 connector/pipewire-screen-audio-connector.sh $out/lib/connector/pipewire-screen-audio-connector.sh
-		install -Dm755 out/pipewire-screenaudio $out/lib/out/pipewire-screenaudio
+		install -Dm755 native/connector/pipewire-screen-audio-connector.sh $out/lib/connector/pipewire-screen-audio-connector.sh
+		install -Dm755 native/connector/virtmic.sh $out/lib/connector/virtmic.sh
 
 		# Firefox manifest
-		sed -i "s|/usr/lib/pipewire-screenaudio|$out/lib|g" native-messaging-hosts/firefox.json
-		install -Dm644 native-messaging-hosts/firefox.json $out/lib/mozilla/native-messaging-hosts/com.icedborn.pipewirescreenaudioconnector.json
+		sed -i "s|/usr/lib/pipewire-screenaudio|$out/lib|g" native/native-messaging-hosts/firefox.json
+		install -Dm644 native/native-messaging-hosts/firefox.json $out/lib/mozilla/native-messaging-hosts/com.icedborn.pipewirescreenaudioconnector.json
 	'';
 }
