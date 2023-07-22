fe8fd1c58059ac6811e17a4f986a7f69f969b732
IceDBorn <github.envenomed@dralias.com>
2023-07-22 06:02:35 +0300

    proton-ge: 8-7 -> 8-8

--- system/programs/self-built/proton-ge.nix
+++ system/programs/self-built/proton-ge.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "proton-ge";
-  version = "GE-Proton8-7";
+  version = "GE-Proton8-8";
 
   src = builtins.fetchTarball {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "1x9z12k0ly413h42vs78wzc97f263clqv5b67wkw7xrzmzzmjc22";
+    sha256 = "0n1w2r99arpj71p4fwd4pivbchzkkv80c6l7703gkg6kzs217psm";
   };
 
   preferLocalBuild = true;
