ed7d1df1bdfdc82f36d91b4afaba23bfb734d7bf
IceDBorn <github.envenomed@dralias.com>
2023-09-01 21:54:26 +0300

    Move nvidia-offload to nvidia.nix and remove nvidia patch

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,14 +1,6 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in {
+{
   hardware = {
     opengl = {
       enable = true;
@@ -16,22 +8,11 @@ in {
         true; # Support Direct Rendering for 32-bit applications (such as Wine) on 64-bit systems
     };
 
-    nvidia = lib.mkIf (config.nvidia.enable && config.nvidia.patch.enable) {
-      package = config.nur.repos.arc.packages.nvidia-patch.override {
-        nvidia_x11 = config.boot.kernelPackages.nvidiaPackages.stable;
-      }; # Patch the driver for nvfbc
-    };
-
     xpadneo.enable =
       !config.xpadneo-unstable.enable; # Enable XBOX Gamepad bluetooth driver
     bluetooth.enable = true;
     uinput.enable = true; # Enable uinput support
   };
-
-  environment.systemPackages =
-    lib.mkIf (config.laptop.enable && config.nvidia.enable) [
-      nvidia-offload
-    ]; # Use nvidia-offload to launch programs using the nvidia GPU
 
   # Set memory limits
   security.pam.loginLimits = [
--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,6 +1,13 @@
 { config, pkgs, lib, ... }:
-
-lib.mkIf config.nvidia.enable {
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in lib.mkIf config.nvidia.enable {
   services.xserver.videoDrivers = [ "nvidia" ]; # Install the nvidia drivers
 
   hardware.nvidia.modesetting.enable = true; # Required for wayland
@@ -9,7 +16,13 @@ lib.mkIf config.nvidia.enable {
     true; # Enable nvidia gpu acceleration for docker
 
   environment.systemPackages =
-    [ pkgs.nvtop-nvidia ]; # Monitoring tool for nvidia GPUs
+    [ pkgs.nvtop-nvidia ] # Monitoring tool for nvidia GPUs
+    ++ (if (config.laptop.enable) then
+      [
+        nvidia-offload
+      ] # Use nvidia-offload to launch programs using the nvidia GPU
+    else
+      [ ]);
 
   # Set nvidia gpu power limit
   systemd.services.nv-power-limit = lib.mkIf config.nvidia.power-limit.enable {
