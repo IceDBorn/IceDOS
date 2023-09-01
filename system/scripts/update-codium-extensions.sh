bafcfd5f70aa1fdef0c870b4acdb01919770f4e3
IceDBorn <github.envenomed@dralias.com>
2023-09-01 04:47:48 +0300

    Sort extensions and remove home override

--- system/scripts/update-codium-extensions.sh
+++ system/scripts/update-codium-extensions.sh
@@ -2,27 +2,27 @@
 
 # list your extensions with: codium --list-extensions
 EXTENSIONS=(
-  "markwylde.vscode-filesize"
-  "pkief.material-icon-theme"
-  "zhuangtongfa.material-theme"
-  "gruntfuggly.todo-tree"
-  "nico-castell.linux-desktop-file"
-  "eamodio.gitlens"
-  "donjayamanne.githistory"
   "codezombiech.gitignore"
-  "ziyasal.vscode-open-in-github"
-  "editorconfig.editorconfig"
   "dbaeumer.vscode-eslint"
+  "donjayamanne.githistory"
+  "eamodio.gitlens"
+  "editorconfig.editorconfig"
   "esbenp.prettier-vscode"
-  "jnoortheen.nix-ide"
-  "formulahendry.code-runner"
   "formulahendry.auto-close-tag"
+  "formulahendry.code-runner"
+  "gruntfuggly.todo-tree"
+  "jnoortheen.nix-ide"
+  "markwylde.vscode-filesize"
   "ms-vscode.references-view"
+  "nico-castell.linux-desktop-file"
+  "pkief.material-icon-theme"
   "timonwong.shellcheck"
+  "zhuangtongfa.material-theme"
+  "ziyasal.vscode-open-in-github"
 )
 
 echo "Installing codium extensions..."
 for extension in "${EXTENSIONS[@]}"; do
   # --force updates the extension if already installed
-  HOME=~/.steam-session codium --force --install-extension "$extension" > /dev/null
+  codium --force --install-extension "$extension" > /dev/null
 done
