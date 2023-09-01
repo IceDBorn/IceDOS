bc02205cc177f697c5f3e985108e67a099e65284
IceDBorn <github.envenomed@dralias.com>
2023-09-01 06:21:18 +0300

    Run vscodium extension updater in parallel and code formatting

--- system/scripts/update-codium-extensions.sh
+++ system/scripts/update-codium-extensions.sh
@@ -2,27 +2,27 @@
 
 # list your extensions with: codium --list-extensions
 EXTENSIONS=(
-  "codezombiech.gitignore"
-  "dbaeumer.vscode-eslint"
-  "donjayamanne.githistory"
-  "eamodio.gitlens"
-  "editorconfig.editorconfig"
-  "esbenp.prettier-vscode"
-  "formulahendry.auto-close-tag"
-  "formulahendry.code-runner"
-  "gruntfuggly.todo-tree"
-  "jnoortheen.nix-ide"
-  "markwylde.vscode-filesize"
-  "ms-vscode.references-view"
-  "nico-castell.linux-desktop-file"
-  "pkief.material-icon-theme"
-  "timonwong.shellcheck"
-  "zhuangtongfa.material-theme"
-  "ziyasal.vscode-open-in-github"
+    "codezombiech.gitignore"
+    "dbaeumer.vscode-eslint"
+    "donjayamanne.githistory"
+    "eamodio.gitlens"
+    "editorconfig.editorconfig"
+    "esbenp.prettier-vscode"
+    "formulahendry.auto-close-tag"
+    "formulahendry.code-runner"
+    "gruntfuggly.todo-tree"
+    "jnoortheen.nix-ide"
+    "markwylde.vscode-filesize"
+    "ms-vscode.references-view"
+    "nico-castell.linux-desktop-file"
+    "pkief.material-icon-theme"
+    "timonwong.shellcheck"
+    "zhuangtongfa.material-theme"
+    "ziyasal.vscode-open-in-github"
 )
 
 echo "Installing codium extensions..."
 for extension in "${EXTENSIONS[@]}"; do
-  # --force updates the extension if already installed
-  codium --force --install-extension "$extension" > /dev/null
+    # --force updates the extension if already installed
+    codium --force --install-extension "$extension" > /dev/null &
 done
