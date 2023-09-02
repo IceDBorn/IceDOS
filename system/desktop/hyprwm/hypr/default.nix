25c406fcfa1e5561efffca35c5949269e2701c91
IceDBorn <github.envenomed@dralias.com>
2023-09-02 23:19:25 +0300

    Install hypr only if it is enabled

--- system/desktop/hyprwm/hypr/default.nix
+++ system/desktop/hyprwm/hypr/default.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   imports = [ ./home/main.nix ./home/work.nix ]; # Setup home manager for hypr
 
-  services.xserver.displayManager.session = [{
-    manage = "desktop";
-    name = "Hypr";
-    start = "Hypr";
-  }];
+  services.xserver.displayManager.session =
+    lib.mkIf config.desktop-environment.hypr.enable [{
+      manage = "desktop";
+      name = "Hypr";
+      start = "Hypr";
+    }];
 
-  environment.systemPackages = with pkgs; [ hypr ];
+  environment.systemPackages =
+    lib.mkIf config.desktop-environment.hypr.enable [ pkgs.hypr ];
 }
