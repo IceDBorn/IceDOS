3e0256aa8a21a054dd43cb430e86900ee7314a6c
IceDBorn <github.envenomed@dralias.com>
2023-09-04 06:13:03 +0300

    Set gamemode settings for maximum performance

--- system/applications/global.nix
+++ system/applications/global.nix
@@ -163,7 +163,18 @@ in {
         unsetopt PROMPT_SP''; # Commands to run on zsh shell initialization
     };
 
-    gamemode.enable = true;
+    # Enable gamemode and set custom settings
+    gamemode = {
+      enable = true;
+      settings = {
+        general.renice = 20;
+        gpu = {
+          apply_gpu_optimisations = 1;
+          nv_powermizer_mode = 1;
+          amd_performance_level = "high";
+        };
+      };
+    };
   };
 
   services = {
