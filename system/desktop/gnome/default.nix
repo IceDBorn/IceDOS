dd4d22dc384217e0bf2646aaf7f4e48402e98129
CrazyStevenz <github.ekta@aleeas.com>
2023-09-04 22:57:22 +0300

    Replace if else with lib.optional

--- system/desktop/gnome/default.nix
+++ system/desktop/gnome/default.nix
@@ -15,23 +15,20 @@
   programs.dconf.enable = config.desktop-environment.gnome.enable;
 
   environment.systemPackages = with pkgs;
-    (if (config.desktop-environment.gnome.enable) then
-      [
-        gnome.dconf-editor # Edit gnome's dconf
-        gnome.gnome-tweaks # Tweaks missing from pure gnome
-        gnomeExtensions.appindicator # Tray icons for gnome
-        gnomeExtensions.pano # Next-gen Clipboard manager
-        gnome-extension-manager # Gnome extensions manager and downloader
-      ] ++ lib.optional config.desktop-environment.gnome.arcmenu.enable
-      gnomeExtensions.arcmenu # Start menu
-      ++ lib.optional config.desktop-environment.gnome.caffeine.enable
-      gnomeExtensions.caffeine # Disable auto suspend and screen blank
-      ++ lib.optional config.desktop-environment.gnome.dash-to-panel.enable
-      gnomeExtensions.dash-to-panel # An icon taskbar for gnome
-      ++ lib.optional config.desktop-environment.gnome.gsconnect.enable
-      gnomeExtensions.gsconnect # KDE Connect implementation for gnome
-    else
-      [ ]);
+    lib.optional config.desktop-environment.gnome.enable [
+      gnome.dconf-editor # Edit gnome's dconf
+      gnome.gnome-tweaks # Tweaks missing from pure gnome
+      gnomeExtensions.appindicator # Tray icons for gnome
+      gnomeExtensions.pano # Next-gen Clipboard manager
+      gnome-extension-manager # Gnome extensions manager and downloader
+    ] ++ lib.optional config.desktop-environment.gnome.arcmenu.enable
+    gnomeExtensions.arcmenu # Start menu
+    ++ lib.optional config.desktop-environment.gnome.caffeine.enable
+    gnomeExtensions.caffeine # Disable auto suspend and screen blank
+    ++ lib.optional config.desktop-environment.gnome.dash-to-panel.enable
+    gnomeExtensions.dash-to-panel # An icon taskbar for gnome
+    ++ lib.optional config.desktop-environment.gnome.gsconnect.enable
+    gnomeExtensions.gsconnect; # KDE Connect implementation for gnome
 
   environment.gnome.excludePackages = with pkgs;
     lib.mkIf config.desktop-environment.gnome.enable [
