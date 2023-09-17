53bdae2bb4bb557f3c013e5946123af94e2a344a
IceDBorn <github.envenomed@dralias.com>
2023-09-17 15:41:01 +0300

    adwaita-for-steam: 1.11 -> 1.12

--- system/applications/self-built/adwaita-for-steam/default.nix
+++ system/applications/self-built/adwaita-for-steam/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "adwaita-for-steam";
-  version = "1.11";
+  version = "1.12";
 
   src = fetchFromGitHub {
     owner = "tkashkin";
     repo = "Adwaita-for-Steam";
     rev = "v${version}";
-    sha256 = "0V0+knSgaiWO6CrOEowM2VIDM+Gi0GBxgzAio4rlvSk=";
+    sha256 = "TAQ27Ka2pIZwlltF1Z4rwK0bb+jtlsDs8RZwiE31c88=";
   };
 
   preferLocalBuild = true;
