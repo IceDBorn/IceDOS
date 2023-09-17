8d165fde226b2fa9e61791d97cb657a8a89be05e
IceDBorn <github.envenomed@dralias.com>
2023-09-17 15:39:09 +0300

    arkenfox-userjs: 115.1 -> 117.0

--- system/applications/self-built/arkenfox-userjs.nix
+++ system/applications/self-built/arkenfox-userjs.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "arkenfox-userjs";
-  version = "115.1";
+  version = "117.0";
 
   src = fetchFromGitHub {
     owner = "arkenfox";
     repo = "user.js";
     rev = version;
-    sha256 = "M523JiwiZR0mwjyjNaojSERFt77Dp75cg0Ifd6wTOdU=";
+    sha256 = "mvQwK11nhZ9yiP9ONeHrP2by5h1UgFSABWkI25YjlN4=";
   };
 
   preferLocalBuild = true;
