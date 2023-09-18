74ebbdd387c773e152525eee23ac5f0dc8ea531b
IceDBorn <github.envenomed@dralias.com>
2023-09-18 15:36:30 +0300

    Fix proton ge not installing

--- system/applications/self-built/proton-ge.nix
+++ system/applications/self-built/proton-ge.nix
@@ -6,7 +6,7 @@ stdenvNoCC.mkDerivation rec {
 
   src = builtins.fetchTarball {
     url =
-      ''https://github.com/GloriousEggroll/proton-ge-custom/releases/download/"GE-Proton"${version}"/"GE-Proton${version}".tar.gz'';
+      "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton${version}/GE-Proton${version}.tar.gz";
     sha256 = "02ny0n8spf3835cxc62hbscrh2np2w7yl387fkgbbwv5hj0zqc2b";
   };
 
