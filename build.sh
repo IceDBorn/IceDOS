2e7c457bce864ae2e9531bf09387111b0941e2d4
jim3692 <jim3692@gmail.com>
2023-09-24 03:31:31 +0300

    Add comments

--- build.sh
+++ build.sh
@@ -4,10 +4,15 @@
 cd "$(cd "$(dirname "$0")" && pwd)" || exit
 printf "$PWD" > .configuration-location
 
+# Copy "/etc/nixos/hardware-configuration.nix" to the project
 [ -f "hardware-configuration.nix" ] && rm -f hardware-configuration.nix
 cp /etc/nixos/hardware-configuration.nix ./
+
+# Set to read-only as the user should update the original file
 chmod 444 hardware-configuration.nix
 
+# Fool flake to use untracked file
+# Source: Development tricks - https://nixos.wiki/wiki/Flakes
 git add --intent-to-add hardware-configuration.nix
 git update-index --skip-worktree hardware-configuration.nix
 
@@ -16,4 +21,6 @@ git update-index --skip-worktree .configuration-location
 
 # Build the configuration
 sudo nixos-rebuild switch --flake .
+
+# Delete the copied hardware-configuration.nix
 rm -f hardware-configuration.nix
