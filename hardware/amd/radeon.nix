788e9c411c3bfd07339754369f7f44adc14e0a68
IceDBorn <github.envenomed@dralias.com>
2023-09-25 20:10:11 +0300

    Replace corectrl with lact

--- hardware/amd/radeon.nix
+++ hardware/amd/radeon.nix
@@ -25,7 +25,23 @@ lib.mkIf config.hardware.gpu.amd.enable {
   '';
 
   environment.systemPackages = with pkgs; [
-    corectrl # GPU overclocking tool
     nvtop-amd # GPU task manager
+    lact
   ];
+
+  # We are creating the lact daemon service manually because the provided one hangs
+  systemd.services.lactd = {
+    enable = true;
+    description = "Radeon GPU monitor";
+    after = [ "syslog.target" "systemd-modules-load.service" ];
+
+    unitConfig = { ConditionPathExists = "${pkgs.lact}/bin/lact"; };
+
+    serviceConfig = {
+      User = "root";
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
 }
