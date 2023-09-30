92bf7c2632595ba42a8bfaf38dd647af4216de89
IceDBorn <github.envenomed@dralias.com>
2023-09-30 17:42:56 +0300

    Stash flake.lock before updating

--- system/applications/global.nix
+++ system/applications/global.nix
@@ -173,9 +173,10 @@ in {
         ssh = "TERM=xterm-256color ssh"; # SSH with colors
         steam-link =
           "killall steam 2> /dev/null ; while ps axg | grep -vw grep | grep -w steam > /dev/null; do sleep 1; done && (nohup steam -pipewire > /dev/null &) 2> /dev/null"; # Kill existing steam process and relaunch steam with the pipewire flag
-        update = "(cd ${
+        update = ''
+          (cd ${
             builtins.readFile ../../.configuration-location
-          } 2> /dev/null || (echo 'Configuration path is invalid. Run build.sh manually to update the path!' && false) && nix flake update && bash build.sh) ; (apx --aur upgrade) ; (bash ~/.config/zsh/proton-ge-updater.sh) ; (bash ~/.config/zsh/steam-library-patcher.sh) ; (bash ~/.config/zsh/update-codium-extensions.sh)"; # Update everything
+          } 2> /dev/null || (echo 'Configuration path is invalid. Run build.sh manually to update the path!' && false) && git stash push -m "flake.lock@$(date +%A-%d-%B-%T)" flake.lock > /dev/null && nix flake update && bash build.sh) ; (apx --aur upgrade) ; (bash ~/.config/zsh/proton-ge-updater.sh) ; (bash ~/.config/zsh/steam-library-patcher.sh) ; (bash ~/.config/zsh/update-codium-extensions.sh)''; # Update everything
         v = "nvim"; # Neovim
         vpn = "ssh -f server@192.168.1.2 'mullvad status'"; # Show VPN status
         vpn-btop = "ssh -t server@192.168.1.2 'bpytop'"; # Show VPN bpytop
