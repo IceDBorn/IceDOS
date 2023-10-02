f0f9c39083617c4a2a85af8a5aa9df8129c8edfa
IceDBorn <github.envenomed@dralias.com>
2023-10-02 07:34:24 +0300

    Separate update scripts between users

--- system/applications/main.nix
+++ system/applications/main.nix
@@ -2,6 +2,27 @@
 { config, pkgs, lib, ... }:
 
 let
+  configuration-location = builtins.readFile ../../.configuration-location;
+
+  update = pkgs.writeShellScriptBin "update" ''
+    stashLock=$(git stash push -m "flake.lock@$(date +%A-%d-%B-%T)" flake.lock > /dev/null)
+      # Navigate to configuration directory
+      cd ${configuration-location} 2> /dev/null || (echo 'Configuration path is invalid. Run build.sh manually to update the path!' && exit 2)
+
+      # Stash the flake lock file
+      if [ $(git stash list | wc -l) -eq 0 ]; then
+        $stashLock
+      else
+        [ -n "$(git diff stash flake.lock)" ] && $stashLock
+      fi
+
+      nix flake update && bash build.sh
+      apx --aur upgrade
+      bash ~/.config/zsh/proton-ge-updater.sh
+      bash ~/.config/zsh/steam-library-patcher.sh
+      bash ~/.config/zsh/update-codium-extensions.sh
+  '';
+
   emulators = with pkgs; [
     cemu # Wii U Emulator
     duckstation # PS1 Emulator
@@ -27,6 +48,7 @@ in lib.mkIf config.system.user.main.enable {
       input-remapper # Remap input device controls
       scanmem # Cheat engine for linux
       stremio # Straming platform
+      update # Update the system configuration
     ] ++ emulators ++ gaming;
 
   # Wayland microcompositor
--- system/applications/work.nix
+++ system/applications/work.nix
@@ -1,6 +1,28 @@
 # PACKAGES INSTALLED ON WORK USER
 { config, pkgs, lib, ... }:
 
-lib.mkIf config.system.user.work.enable {
-  users.users.${config.system.user.work.username}.packages = with pkgs; [ ];
+let
+  configuration-location = builtins.readFile ../../.configuration-location;
+
+  update = pkgs.writeShellScriptBin "update" ''
+    stashLock=$(git stash push -m "flake.lock@$(date +%A-%d-%B-%T)" flake.lock > /dev/null)
+      # Navigate to configuration directory
+      cd ${configuration-location} 2> /dev/null || (echo 'Configuration path is invalid. Run build.sh manually to update the path!' && exit 2)
+
+      # Stash the flake lock file
+      if [ $(git stash list | wc -l) -eq 0 ]; then
+        $stashLock
+      else
+        [ -n "$(git diff stash flake.lock)" ] && $stashLock
+      fi
+
+      nix flake update && bash build.sh
+      apx --aur upgrade
+      bash ~/.config/zsh/update-codium-extensions.sh
+  '';
+in lib.mkIf config.system.user.work.enable {
+  users.users.${config.system.user.work.username}.packages = with pkgs;
+    [
+      update # Update the system configuration
+    ];
 }
