a8ddaabcc5d5807d04aa063b7a8a2085df7258c5
IceDBorn <github.envenomed@dralias.com>
2023-10-19 23:36:16 +0300

    Fix work httpd server redirections

--- system/applications/work.nix
+++ system/applications/work.nix
@@ -8,31 +8,39 @@ let
   update = pkgs.writeShellScriptBin "update" "rebuild 1 ${stashLock} 0";
   shellScripts = [ update ];
 
-  gitLocation = "/home/${config.system.user.work.username}/git";
-  projectName = "fromcreta";
+  gitLocation = "/home/${config.system.user.work.username}/git/";
+  projectName = "smart-trade";
 in lib.mkIf config.system.user.work.enable {
   users.users.${config.system.user.work.username}.packages = with pkgs;
     [
-      apacheHttpd
-      phpPackages.composer
-      jetbrains.datagrip
-      php
-      phpPackages.composer
-      google-chrome-dev
+      apacheHttpd # HTTP Server
+      beekeeper-studio # Database manager
+      google-chrome-dev # Dev browser
+      php # Programming language for websites
+      phpPackages.composer # Package manager for PHP
     ] ++ shellScripts;
 
   services = {
     httpd = {
       enable = true;
       user = config.system.user.work.username;
-      phpPackage = inputs.phps.packages.x86_64-linux.php73;
+      phpPackage = inputs.phps.packages.x86_64-linux.php74;
       enablePHP = true;
-      virtualHosts = {
-        localhost = {
-          documentRoot = "${gitLocation}/${projectName}";
-          locations."/".index = "index.php";
-        };
-      };
+      extraConfig = ''
+        <VirtualHost *:80>
+          ServerName ${config.system.user.work.username}.localhost
+          ServerAdmin ${config.system.user.work.username}@localhost
+          DocumentRoot ${gitLocation}
+          Alias /burkani ${gitLocation}${projectName}
+          <Directory ${gitLocation}>
+            AllowOverride all
+            Options Indexes FollowSymLinks MultiViews
+            Order Deny,Allow
+            Allow from all
+            Require all granted
+          </Directory>
+        </VirtualHost>
+      '';
     };
 
     mysql = {
