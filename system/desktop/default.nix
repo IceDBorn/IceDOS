3dcb9f3a8dec71f957d32071c08e59bb99b1ee8d
IceDBorn <git.roving782@dralias.com>
2023-10-19 09:50:25 +0300

    Fix build when gnome monitor config is missing

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -6,8 +6,7 @@ let
   else
     config.system.user.work.username;
 
-  monitorsConfig = pkgs.writeText "gdm_monitors.xml"
-    (builtins.readFile /home/${selectedUser}/.config/monitors.xml);
+  monitorConfig = "/home/${selectedUser}/.config/monitors.xml";
 in {
   imports = [ ./home/main.nix ./home/work.nix ]; # Setup home manager
 
@@ -54,10 +53,13 @@ in {
     };
   };
 
-  # Set GDM monitors config, matching the main user's config
-  systemd.tmpfiles.rules =
-    lib.mkIf (config.system.user.main.enable || config.system.user.work.enable)
-    [ "L+ /run/gdm/.config/monitors.xml - - - - ${monitorsConfig}" ];
+  # Set GDM monitors config, matching the user's config
+  systemd.tmpfiles.rules = lib.mkIf (lib.pathExists monitorConfig
+    && (config.system.user.main.enable || config.system.user.work.enable)) [
+      "L+ /run/gdm/.config/monitors.xml - - - - ${
+        builtins.readFile monitorConfig
+      }"
+    ];
 
   # Workaround for GDM autologin
   systemd.services = {
