2c33ecfe0a9d0783bcfb90f5c6904fa0973e85a4
CrazyStevenz <github.ekta@aleeas.com>
2023-11-20 18:19:37 +0200

    Enable some experimental firefox options

--- system/applications/configs/firefox/user.js
+++ system/applications/configs/firefox/user.js
@@ -1,5 +1,11 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.theme.dark-private-windows", false);
 user_pref("gnomeTheme.hideWebrtcIndicator", true);
+user_pref("image.jxl.enabled", true); // Enable JPEG XL support
+user_pref("media.ffmpeg.vaapi.enabled", true); // Enable VA-API hard accelaration
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("widget.wayland.fractional-scale.enabled", true);
+
+// USELESS OPTIONS FOR REFERENCE
+// user_pref("gfx.webrender.all", true); This has been the default since 2021, so no reason to force it here
+// user_pref("media.hardware-video-decoding.force-enabled", true); Alias for media.ffmpeg.vaapi.enabled, no need to enable
