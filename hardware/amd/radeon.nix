54974628c0e781fb518e2873aa8985c815f04eb3
CrazyStevenz <github.ekta@aleeas.com>
2023-12-12 23:08:31 +0200

    Remove corectrl

--- hardware/amd/radeon.nix
+++ hardware/amd/radeon.nix
@@ -3,27 +3,6 @@
 lib.mkIf config.hardware.gpu.amd {
   boot.initrd.kernelModules = [ "amdgpu" ]; # Use the amdgpu drivers upon boot
 
-  programs.corectrl = {
-    enable = true;
-    gpuOverclock = {
-      enable = true;
-      ppfeaturemask = "0xffffffff"; # Unlock all gpu controls
-    };
-  };
-
-  # Do not ask for password when launching corectrl
-  security.polkit.extraConfig = ''
-    polkit.addRule(function (action, subject) {
-      if ((action.id == "org.corectrl.helper.init" ||
-          action.id == "org.corectrl.helperkiller.init") &&
-          subject.local == true &&
-          subject.active == true &&
-          subject.isInGroup("users")) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
-
   environment.systemPackages = with pkgs; [
     nvtop-amd # GPU task manager
     lact # GPU overclocking tool
