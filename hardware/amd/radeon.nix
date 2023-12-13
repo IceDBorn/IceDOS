273746034414fc006387668063bde5d5c8fb62ba
CrazyStevenz <github.ekta@aleeas.com>
2023-12-13 15:41:25 +0200

    Directly add ppfeaturemask to kernel params

--- hardware/amd/radeon.nix
+++ hardware/amd/radeon.nix
@@ -1,14 +1,10 @@
 { pkgs, lib, config, ... }:
 
 lib.mkIf config.hardware.gpu.amd {
-  boot.initrd.kernelModules = [ "amdgpu" ]; # Use the amdgpu drivers upon boot
-
-  programs.corectrl = {
-    gpuOverclock = {
-      enable = true;
-      ppfeaturemask = "0xffffffff"; # Unlock all gpu controls
-    };
-  };
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ]; # Use the amdgpu drivers upon boot
+    kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ]; # Unlock all gpu controls
+  }
 
   environment.systemPackages = with pkgs; [
     nvtop-amd # GPU task manager
