708f37890c189aef70b6d9433a408ad3e8772667
CrazyStevenz <github.ekta@aleeas.com>
2023-12-13 15:27:25 +0200

    Revert removal of gpu overclock ppfeaturemask

--- hardware/amd/radeon.nix
+++ hardware/amd/radeon.nix
@@ -3,6 +3,13 @@
 lib.mkIf config.hardware.gpu.amd {
   boot.initrd.kernelModules = [ "amdgpu" ]; # Use the amdgpu drivers upon boot
 
+  programs.corectrl = {
+    gpuOverclock = {
+      enable = true;
+      ppfeaturemask = "0xffffffff"; # Unlock all gpu controls
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     nvtop-amd # GPU task manager
     lact # GPU overclocking tool
