fa0bbdefb1f34bdc905d23e3a7640444e77a2300
CrazyStevenz <github.ekta@aleeas.com>
2023-12-19 22:54:45 +0200

    Dynamically determine system hostname

--- flake.nix
+++ flake.nix
@@ -16,24 +16,25 @@
 
   outputs = { self, nixpkgs, hyprland, home-manager, nur, pipewire-screenaudio
     , steam-session, phps }@inputs: {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          {
-            options = with nixpkgs.lib; {
-              configurationLocation = mkOption {
-                type = types.str;
-                default = toString ./.;
+      nixosConfigurations.${nixpkgs.lib.fileContents "/etc/hostname"} =
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            {
+              options = with nixpkgs.lib; {
+                configurationLocation = mkOption {
+                  type = types.str;
+                  default = toString ./.;
+                };
               };
-            };
-          }
-          nur.nixosModules.nur
-          home-manager.nixosModules.home-manager
-          hyprland.nixosModules.default
-          steam-session.nixosModules.default
-          ./configuration.nix
-        ];
-      };
+            }
+            nur.nixosModules.nur
+            home-manager.nixosModules.home-manager
+            hyprland.nixosModules.default
+            steam-session.nixosModules.default
+            ./configuration.nix
+          ];
+        };
     };
 }
