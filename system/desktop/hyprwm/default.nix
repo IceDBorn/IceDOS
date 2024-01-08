2d6553532561475bee1fcd2cc993910957b212ee
IceDBorn <git.outsider841@simplelogin.fr>
2024-01-08 09:45:40 +0200

    Replace blueberry with overskride

--- system/desktop/hyprwm/default.nix
+++ system/desktop/hyprwm/default.nix
@@ -7,17 +7,15 @@
     ./configs/swaync/config.nix
   ]; # Setup home manager for hypr and hyprland
 
-  programs =
-    lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
-      nm-applet.enable = true; # Network manager tray icon
-      kdeconnect.enable = true; # Connect phone to PC
-    };
+  programs = lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
+    nm-applet.enable = true; # Network manager tray icon
+    kdeconnect.enable = true; # Connect phone to PC
+  };
 
   environment =
     lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
       systemPackages = with pkgs; [
         baobab # Disk usage analyser
-        blueberry # Bluetooth manager
         feh # Minimal image viewer
         gnome-online-accounts # Nextcloud integration
         gnome.file-roller # Archive file manager
@@ -30,32 +28,30 @@
         gnome.nautilus # File manager
         grim # Screenshot tool
         networkmanagerapplet # Network manager tray icon
+        overskride # Bluetooth manager
         polkit_gnome # Polkit manager
         rofi-wayland # App launcher
         swappy # Edit screenshots
         swaynotificationcenter # Notification daemon
       ];
 
-      etc = lib.mkIf
-        (config.desktop.hyprland.enable || config.desktop.hypr) {
-          "polkit-gnome".source =
-            "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          "kdeconnectd".source =
-            "${pkgs.libsForQt5.kdeconnect-kde}/libexec/kdeconnectd";
-        };
+      etc = lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
+        "polkit-gnome".source =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        "kdeconnectd".source =
+          "${pkgs.libsForQt5.kdeconnect-kde}/libexec/kdeconnectd";
+      };
     };
 
-  services =
-    lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
-      dbus.enable = true;
-      gvfs.enable = true; # Needed for nautilus
-      gnome.gnome-keyring.enable = true;
-    };
+  services = lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) {
+    dbus.enable = true;
+    gvfs.enable = true; # Needed for nautilus
+    gnome.gnome-keyring.enable = true;
+  };
 
   security = {
     polkit.enable =
-      lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr)
-      true;
+      lib.mkIf (config.desktop.hyprland.enable || config.desktop.hypr) true;
     pam.services.login.enableGnomeKeyring = true;
   };
 
--- system/desktop/hyprwm/hyprland/configs/waybar/config.nix
+++ system/desktop/hyprwm/hyprland/configs/waybar/config.nix
@@ -82,7 +82,7 @@ in {
             "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}",
             "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}",
             "tooltip-format-enumerate-connected-battery": "{device_alias}\t{device_address}\t({device_battery_percentage})",
-            "on-click": "blueberry"
+            "on-click": "overskride"
           },
 
           "tray": {
