0190e2ffddc95402f555bcc826deab5986641a1a
IceDBorn <github.envenomed@dralias.com>
2024-01-29 10:24:06 +0200

    proton-ge: 8-27 -> 8-28

--- system/applications/self-built/proton-ge.nix
+++ system/applications/self-built/proton-ge.nix
@@ -2,12 +2,12 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "proton-ge";
-  version = "8-27";
+  version = "8-28";
 
   src = builtins.fetchTarball {
     url =
       "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton${version}/GE-Proton${version}.tar.gz";
-    sha256 = "00nl7xncffq786kvpvqz8nbv2xiq8w0v4zvm2g6xdsxk1m69ps31";
+    sha256 = "1a6l1pdd16mjzf3pz0rdh7rmlag517rq18d5y0chr29qlhkqnd51";
   };
 
   preferLocalBuild = true;
