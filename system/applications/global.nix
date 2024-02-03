68fe813286a0e5be5d58c56be9adb5ca72f55d21
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-03 22:16:46 +0200

    Fix rebuild command not executing

--- system/applications/global.nix
+++ system/applications/global.nix
@@ -56,9 +56,9 @@ let
       fi
 
       # Update apx packages
-      if [ $ARG4 -eq 1 ]; then
-        # apx --aur upgrade
-      fi
+      # if [ $ARG4 -eq 1 ]; then
+      #   apx --aur upgrade
+      # fi
 
       # Update commands for all users
       bash ~/.config/zsh/update-codium-extensions.sh
