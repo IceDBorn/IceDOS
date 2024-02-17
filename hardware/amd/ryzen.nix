bf369f4f4d2415994fe49b5c2c84982bde694fe9
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-17 21:01:12 +0200

    config: introduce variables for ryzen module

--- hardware/amd/ryzen.nix
+++ hardware/amd/ryzen.nix
@@ -1,36 +1,29 @@
 { pkgs, lib, config, ... }:
 
-lib.mkIf config.hardware.cpu.amd.enable {
-  boot = lib.mkMerge [
-    {
-      kernelModules = [ "msr" "zenpower" ];
-      extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
-    }
-
-    # for older kernels, see https://github.com/NixOS/nixos-hardware/blob/c256df331235ce369fdd49c00989fdaa95942934/common/cpu/amd/pstate.nix
-    (lib.mkIf
-      (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.3") {
-        kernelParams = [ "amd_pstate=active" ];
-      })
-  ];
+let
+  cfg = config.hardware.cpu.amd;
+  kernelPackages = config.boot.kernelPackages;
+in lib.mkIf cfg.enable {
+  boot = {
+    kernelModules = [ "msr" "zenpower" ];
+    extraModulePackages = with kernelPackages; [ zenpower ];
+  };
 
   hardware.cpu.amd.updateMicrocode = true;
 
   # Ryzen cpu control
-  systemd.services.zenstates =
-    lib.mkIf config.hardware.cpu.amd.undervolt.enable {
-      enable = true;
-      description = "Ryzen Undervolt";
-      after = [ "syslog.target" "systemd-modules-load.service" ];
+  systemd.services.zenstates = lib.mkIf cfg.undervolt.enable {
+    enable = true;
+    description = "Ryzen Undervolt";
+    after = [ "syslog.target" "systemd-modules-load.service" ];
 
-      unitConfig = { ConditionPathExists = "${pkgs.zenstates}/bin/zenstates"; };
+    unitConfig = { ConditionPathExists = "${pkgs.zenstates}/bin/zenstates"; };
 
-      serviceConfig = {
-        User = "root";
-        ExecStart =
-          "${pkgs.zenstates}/bin/zenstates ${config.hardware.cpu.amd.undervolt.value}";
-      };
-
-      wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "root";
+      ExecStart = "${pkgs.zenstates}/bin/zenstates ${cfg.undervolt.value}";
     };
+
+    wantedBy = [ "multi-user.target" ];
+  };
 }
