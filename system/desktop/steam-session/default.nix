baa1ba7225dec31e7e502cbfef1522b10379f696
IceDBorn <github.envenomed@dralias.com>
2024-02-19 10:47:21 +0200

    steam-session: add more defaults and checks

--- system/desktop/steam-session/default.nix
+++ system/desktop/steam-session/default.nix
@@ -3,24 +3,28 @@
 let
   cfg = config.applications.steam.session;
   steamUser = config.system.user.main.username;
+  amdGpu = config.hardware.gpu.amd.enable;
 in {
   # Unlock password using steam deck controller
   imports = [ modules/deckbd-wrapper.nix ];
 
   jovian = {
-    decky-loader.enable = cfg.decky;
+    decky-loader.enable = (cfg.enable && cfg.decky);
 
-    devices.steamdeck = lib.mkIf cfg.steamdeck {
+    devices.steamdeck = lib.mkIf (cfg.enable && cfg.steamdeck) {
       enable = true;
       enableGyroDsuService = true;
       autoUpdate = true;
     };
 
-    steam = {
+    steam = lib.mkIf cfg.enable {
       enable = true;
       autoStart = cfg.autoStart.enable;
       desktopSession = cfg.autoStart.desktopSession;
       user = steamUser;
     };
+
+    hardware.has.amd.gpu = (cfg.enable && amdGpu);
+    steamos.useSteamOSConfig = cfg.enable;
   };
 }
