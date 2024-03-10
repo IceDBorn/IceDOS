5c3587f446c8e5b8ecec1850729e875f993cad1e
IceDBorn <git.outsider841@simplelogin.fr>
2024-03-11 00:23:12 +0200

    hardware: update mounts

--- hardware/mounts.nix
+++ hardware/mounts.nix
@@ -1,22 +1,24 @@
 { lib, config, ... }:
 
 lib.mkIf config.hardware.mounts {
-  fileSystems."/mnt/Games" = {
+  fileSystems."/mnt/games" = {
     device = "/dev/disk/by-uuid/89afb683-90b4-4291-82af-5d3238c8bd3d";
     fsType = "btrfs";
     options = lib.mkIf (config.hardware.btrfsCompression.enable
       && config.hardware.btrfsCompression.mounts) [ "compress=zstd" ];
   };
 
-  fileSystems."/mnt/Storage" = {
+  fileSystems."/mnt/storage" = {
     device = "/dev/disk/by-uuid/4b2d6cf0-f73a-4b4a-820a-db5b5aa17efa";
     fsType = "btrfs";
     options = lib.mkIf (config.hardware.btrfsCompression.enable
       && config.hardware.btrfsCompression.mounts) [ "compress=zstd" ];
   };
 
-  fileSystems."/mnt/Windows" = {
-    device = "/dev/disk/by-uuid/8AAE4B96AE4B79A9";
-    fsType = "ntfs";
+  fileSystems."/mnt/ssdgames" = {
+    device = "/dev/disk/by-uuid/040329ae-685d-4ba6-8bdd-e0a9785f9672";
+    fsType = "btrfs";
+    options = lib.mkIf (config.hardware.btrfsCompression.enable
+      && config.hardware.btrfsCompression.mounts) [ "compress=zstd" ];
   };
 }
