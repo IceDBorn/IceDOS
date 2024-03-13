1ffb0c2fda65ccf262078f30c9b86688f4beba40
IceDBorn <git.outsider841@simplelogin.fr>
2024-03-14 01:14:50 +0200

    nvchad: add mark all command

--- system/applications/configs/nvchad/mappings.lua
+++ system/applications/configs/nvchad/mappings.lua
@@ -2,18 +2,19 @@ local M = {}
 
 M.abc = {
   v = {
-    ["<Tab>"] = { ">gv", "Indent" },
     ["<S-Tab>"] = { "<gv", "Unindent" },
+    ["<Tab>"] = { ">gv", "Indent" },
     ["<leader>s"] = { "<ESC> <cmd> Sort <CR>", "Sort selection" },
   },
   n = {
-    ["<leader>fr"] = { "<cmd> Spectre <CR>", "Find and Replace" },
     ["<leader>ff"] = { "<cmd> Telescope live_grep <CR>", "Find" },
+    ["<leader>fr"] = { "<cmd> Spectre <CR>", "Find and Replace" },
     ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Source control" },
+    ["<leader>ma"] = { "ggVG", "Mark all" },
     ["C-h"] = { "<cmd> TmuxNavigateLeft <CR>", "Focus left" },
-    ["C-l"] = { "<cmd> TmuxNavigateRight <CR>", "Focus right" },
     ["C-j"] = { "<cmd> TmuxNavigateDown <CR>", "Focus down" },
     ["C-k"] = { "<cmd> TmuxNavigateUp <CR>", "Focus up" },
+    ["C-l"] = { "<cmd> TmuxNavigateRight <CR>", "Focus right" },
   },
   i = {
     ["jk"] = { "<ESC>", "Escape insert mode", opts = { nowait = true } },
