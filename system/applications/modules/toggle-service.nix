c9bb54a074b232d2c05c43c3d43d1ac829d0aff8
IceDBorn <github.envenomed@dralias.com>
2024-03-21 11:54:55 +0200

    toggle-service: make script more verbose

--- system/applications/modules/toggle-service.nix
+++ system/applications/modules/toggle-service.nix
@@ -1,9 +1,24 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "toggle-service" ''
-  if systemctl is-active --quiet "$1"; then
-      sudo systemctl stop "$1"
+  SERVICE="$1"
+
+  # Retain sudo
+  trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+
+  if [[ ! "$SERVICE" == *".service"* ]]; then
+      SERVICE="''${SERVICE}.service"
+  fi
+
+  if systemctl list-unit-files "$SERVICE" &>/dev/null; then
+      if systemctl is-active --quiet "$1"; then
+          echo "Stopping \"$SERVICE\"..."
+          sudo systemctl stop "$SERVICE"
+      else
+          echo "Starting \"$SERVICE\"..."
+          sudo systemctl start "$SERVICE"
+      fi
   else
-      sudo systemctl start "$1"
+      echo "\"$SERVICE\" does not exist..."
   fi
 ''
