d76e1f2c6c3a306af37545b0e69da279a62bab4f
IceDBorn <git.outsider841@simplelogin.fr>
2024-04-14 20:47:10 +0300

    laptop: remove cpupower-gui (not functioning)

--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -11,12 +11,7 @@ let
   cfg = config.icedos.hardware;
 in
 mkIf (cfg.laptop.enable) {
-  services.cpupower-gui.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    brightnessctl
-    cpupower-gui
-  ];
+  environment.systemPackages = [ pkgs.brightnessctl ];
 
   hardware.nvidia.prime = mkIf (cfg.gpu.nvidia.enable) {
     offload.enable = true;
