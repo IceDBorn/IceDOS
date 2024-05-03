cd0479a727e0f19aa953066eee0834eee4c8a68a
IceDBorn <git.outsider841@simplelogin.fr>
2024-05-03 16:13:43 +0300

    hyprland: fix wrong primary xwayland monitor after suspend

--- system/desktop/hyprland/configs/hypridle.nix
+++ system/desktop/hyprland/configs/hypridle.nix
@@ -4,62 +4,72 @@ let
   inherit (lib) attrNames filter foldl';
 
   cfg = config.icedos;
-
-  mapAttrsAndKeys = callback: list:
-    (foldl' (acc: value: acc // (callback value)) { } list);
-in {
-  home-manager.users = let
-    users = filter (user: cfg.system.user.${user}.enable == true)
-      (attrNames cfg.system.user);
-  in mapAttrsAndKeys (user:
+  idle = cfg.system.user.${user}.desktop.idle;
+  mapAttrsAndKeys = callback: list: (foldl' (acc: value: acc // (callback value)) { } list);
+  monitor = cfg.hardware.monitors.main.name;
+in
+{
+  home-manager.users =
     let
-      username = cfg.system.user.${user}.username;
-      idle = cfg.system.user.${user}.desktop.idle;
+      users = filter (user: cfg.system.user.${user}.enable == true) (attrNames cfg.system.user);
+    in
+    mapAttrsAndKeys (
+      user:
+      let
+        username = cfg.system.user.${user}.username;
+      in
+      {
+        ${username}.home.file.".config/hypr/hypridle.conf".text = ''
+          general {
+              lock_cmd = pidof hyprlock || hyprlock
+              before_sleep_cmd = loginctl lock-session
+              after_sleep_cmd = hyprctl dispatch dpms on && xrandr --output "${monitor}" --primary
+          }
 
-      lock = if (idle.lock.enable) then ''
-        listener {
-            timeout = ${idle.lock.seconds}
-            on-timeout = hyprlock-wrapper lock
-        }
-      '' else
-        "";
+          # Lower brightness
+          listener {
+              timeout = ${cfg.desktop.hyprland.lock.secondsToLowerBrightness}
+              on-timeout = brightnessctl -s set 10 && brightnessctl -sd rgb:kbd_backlight set 0
+              on-resume = brightnessctl -r && brightnessctl -rd rgb:kbd_backlight
+          }
 
-      disableMonitors = if (idle.disableMonitors.enable) then ''
-        listener {
-            timeout = ${idle.disableMonitors.seconds}
-            on-timeout = hyprlock-wrapper off
-            on-resume = hyprctl dispatch dpms on
-        }
-      '' else
-        "";
+          ${
+            if (idle.lock.enable) then
+              ''
+                listener {
+                    timeout = ${idle.lock.seconds}
+                    on-timeout = hyprlock-wrapper lock
+                }
+              ''
+            else
+              ""
+          }
 
-      suspend = if (idle.suspend.enable) then ''
-        listener {
-            timeout = ${idle.suspend.seconds}
-            on-timeout = hyprlock-wrapper suspend
-        }
-      '' else
-        "";
-    in {
-      ${username}.home.file.".config/hypr/hypridle.conf".text = ''
-        general {
-            lock_cmd = pidof hyprlock || hyprlock
-            before_sleep_cmd = loginctl lock-session
-            after_sleep_cmd = hyprctl dispatch dpms on
-        }
+          ${
+            if (idle.disableMonitors.enable) then
+              ''
+                listener {
+                    timeout = ${idle.disableMonitors.seconds}
+                    on-timeout = hyprlock-wrapper off
+                    on-resume = hyprctl dispatch dpms on
+                }
+              ''
+            else
+              ""
+          }
 
-        # Lower brightness
-        listener {
-            timeout = ${cfg.desktop.hyprland.lock.secondsToLowerBrightness}
-            on-timeout = brightnessctl -s set 10 && brightnessctl -sd rgb:kbd_backlight set 0
-            on-resume = brightnessctl -r && brightnessctl -rd rgb:kbd_backlight
-        }
-
-        ${lock}
-
-        ${disableMonitors}
-
-        ${suspend}
-      '';
-    }) users;
+          ${
+            if (idle.suspend.enable) then
+              ''
+                listener {
+                    timeout = ${idle.suspend.seconds}
+                    on-timeout = hyprlock-wrapper suspend
+                }
+              ''
+            else
+              ""
+          }
+        '';
+      }
+    ) users;
 }
