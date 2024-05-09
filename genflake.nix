ee521fe864c5ee5174ad7302f32e67b1982eda81
IceDBorn <github.envenomed@dralias.com>
2024-05-09 15:34:25 +0300

    hyprland: update flake url

--- genflake.nix
+++ genflake.nix
@@ -44,7 +44,7 @@ in
         ${
           if (hyprland) then
             ''
-              hyprland.url = "github:hyprwm/Hyprland";
+              hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
             ''
           else
             ""
