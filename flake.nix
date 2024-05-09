2e7611fbf7efbc5c6660f1e1bd41792ac3edbf96
IceDBorn <git.outsider841@simplelogin.fr>
2024-05-09 22:18:42 +0300

    kernel: 6.8.8 -> 6.8.9

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     };
 
     # Apps
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     phps = {
       url = "github:fossar/nix-phps";
