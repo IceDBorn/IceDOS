cdeebbba072704871bc3b8ec741c08e7cf43523e
IceDBorn <git.outsider841@simplelogin.fr>
2024-07-17 20:40:16 +0300

    gamemode: fix apps not launching

--- system/applications/modules/gamemode.nix
+++ system/applications/modules/gamemode.nix
@@ -1,13 +1,21 @@
+{ pkgs, ... }:
+
 {
   programs.gamemode = {
     enable = true;
 
     settings = {
       general.renice = 20;
+
       gpu = {
-        apply_gpu_optimisations = 1;
+        apply_gpu_optimisations = "accept-responsibility";
         nv_powermizer_mode = 1;
         amd_performance_level = "high";
+      };
+
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'Gamemode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'Gamemode ended'";
       };
     };
   };
