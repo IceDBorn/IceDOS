c7e23e0185aaf6015f3e1234ec7a298f2eeff59c
IceDBorn <git.outsider841@simplelogin.fr>
2024-08-08 13:19:42 +0300

    nix-shell: use nixpkgs from flake inputs

--- system/applications/default.nix
+++ system/applications/default.nix
@@ -1,12 +1,28 @@
-{ config, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 
 let
+  inherit (lib) mapAttrs mapAttrsToList;
   cfg = config.icedos.system.config;
 in
 {
   imports = [ ./global.nix ];
 
   nix = {
+    # Automatic garbage collection
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
+
+    # Use flake's nixpkgs input for nix-shell
+    nixPath = mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
+    registry = mapAttrs (_: v: { flake = v; }) inputs;
+
     settings = {
       # Use hard links to save space (slows down package manager)
       auto-optimise-store = true;
@@ -17,18 +33,10 @@ in
         "flakes"
       ];
     };
-
-    # Automatic garbage collection
-    gc = {
-      automatic = true;
-      dates = "weekly";
-    };
   };
 
-  nixpkgs.config = {
-    # Allow proprietary packages
-    allowUnfree = true;
-  };
+  # Allow proprietary packages
+  nixpkgs.config.allowUnfree = true;
 
   # Versioning system
   environment.etc."icedos-version".text = cfg.version;
