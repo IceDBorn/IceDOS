430b7f5ac002fd73197a1b8c5743926a9410f791
IceDBorn <git.outsider841@simplelogin.fr>
2024-10-19 20:33:39 +0300

    mounts: update uuids

--- hardware/mounts.nix
+++ hardware/mounts.nix
@@ -8,19 +8,19 @@ let
 in
 mkIf (cfg.mounts) {
   fileSystems."/mnt/games" = {
-    device = "/dev/disk/by-uuid/89afb683-90b4-4291-82af-5d3238c8bd3d";
+    device = "/dev/disk/by-uuid/040329ae-685d-4ba6-8bdd-e0a9785f9672";
+    fsType = "btrfs";
+    options = mkIf (btrfsCompression.enable && btrfsCompression.mounts) [ "compress=zstd" ];
+  };
+
+  fileSystems."/mnt/games2" = {
+    device = "/dev/disk/by-uuid/60876d91-f863-45db-88c5-4c707879588f";
     fsType = "btrfs";
     options = mkIf (btrfsCompression.enable && btrfsCompression.mounts) [ "compress=zstd" ];
   };
 
   fileSystems."/mnt/storage" = {
-    device = "/dev/disk/by-uuid/4b2d6cf0-f73a-4b4a-820a-db5b5aa17efa";
-    fsType = "btrfs";
-    options = mkIf (btrfsCompression.enable && btrfsCompression.mounts) [ "compress=zstd" ];
-  };
-
-  fileSystems."/mnt/ssdgames" = {
-    device = "/dev/disk/by-uuid/040329ae-685d-4ba6-8bdd-e0a9785f9672";
+    device = "/dev/disk/by-uuid/89730200-942d-4a5c-893f-0196c87435d2";
     fsType = "btrfs";
     options = mkIf (btrfsCompression.enable && btrfsCompression.mounts) [ "compress=zstd" ];
   };
