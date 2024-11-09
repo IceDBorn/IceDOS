1bc861c48e31af7b961f7ed7d54754af4ec44400
jim3692 <jim3692@gmail.com>
2024-11-10 00:19:16 +0200

    module: move gnome arcmenu to module

---
+++ system/applications/modules/arcmenu.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  cfg = config.icedos;
+in with lib; mkIf cfg.desktop.gnome.extensions.arcmenu {
+  environment.systemPackages = [ pkgs.gnomeExtensions.arcmenu ];
+
+  home-manager.users = mapAttrs (user: _: {
+    dconf.settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [ "arcmenu@arcmenu.com" ];
+      };
+
+      "org/gnome/shell/extensions/arcmenu" = {
+        distro-icon = 6;
+        menu-button-icon = "Distro_Icon"; # Use arch icon
+        multi-monitor = true;
+        menu-layout = "Windows";
+        windows-disable-frequent-apps = true;
+        windows-disable-pinned-apps = !cfg.system.users.${user}.desktop.gnome.pinnedApps.arcmenu.enable;
+        pinned-apps =
+          with inputs.home-manager.lib.hm.gvariant;
+          (lib.map (s: [
+            (mkDictionaryEntry [
+              "id"
+              s
+            ])
+          ]) cfg.system.users.${user}.desktop.gnome.pinnedApps.arcmenu.list);
+      };
+    };
+  }) cfg.system.users;
+}
