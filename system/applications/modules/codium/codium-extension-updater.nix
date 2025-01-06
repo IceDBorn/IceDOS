d29ba36fc499e602cade8d265c51b6c150dd9bb9
jim3692 <31220180+jim3692@users.noreply.github.com>
2025-01-06 16:17:05 +0000

    formating: replace tabs with spaces (#234)

--- system/applications/modules/codium/codium-extension-updater.nix
+++ system/applications/modules/codium/codium-extension-updater.nix
@@ -13,17 +13,17 @@ pkgs.writeShellScriptBin "update-codium-extensions" ''
 
   i=0
   while (( i < GOAL )); do
-  	JOBS=$(<"$JOBS_LOG")
+    JOBS=$(<"$JOBS_LOG")
 
-  	if (( JOBS > THREADS )); then sleep 0.5 && continue; fi
+    if (( JOBS > THREADS )); then sleep 0.5 && continue; fi
 
-  	echo -ne "Updating codium extensions ($(( i + 1 ))/$GOAL)... \r"
+    echo -ne "Updating codium extensions ($(( i + 1 ))/$GOAL)... \r"
 
-  	(
-  		echo $((JOBS + 1)) > "$JOBS_LOG" &&
-  		JOBS=$(<"$JOBS_LOG") &&
-  		codium --force --install-extension "''${EXTENSIONS[$i]}" > /dev/null &&
-  		echo $((JOBS - 1)) > "$JOBS_LOG"
-  	) & i=$(( i + 1 )) && sleep 0.1
+    (
+      echo $((JOBS + 1)) > "$JOBS_LOG" &&
+      JOBS=$(<"$JOBS_LOG") &&
+      codium --force --install-extension "''${EXTENSIONS[$i]}" > /dev/null &&
+      echo $((JOBS - 1)) > "$JOBS_LOG"
+    ) & i=$(( i + 1 )) && sleep 0.1
   done
 ''
