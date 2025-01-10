7d10fb5b18c73bb2597c82aaa40505e476a715a0
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-10 21:44:10 +0200

    container-manager: remove xhost (use nix-shell)

--- system/applications/modules/container-manager/default.nix
+++ system/applications/modules/container-manager/default.nix
@@ -10,11 +10,7 @@ let
   cfg = config.icedos.system.virtualisation.containerManager;
 in
 mkIf (cfg.enable) {
+  environment.systemPackages = with pkgs; [ distrobox ];
   virtualisation.docker.enable = !cfg.usePodman;
   virtualisation.podman.enable = cfg.usePodman;
-
-  environment.systemPackages = with pkgs; [
-    distrobox
-    xorg.xhost
-  ];
 }
