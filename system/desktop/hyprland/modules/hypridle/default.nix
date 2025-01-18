dcc97e3e308372f12f6143d4c64b5bba3ffd7b32
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-18 05:40:42 +0200

    hypridle: remove unused let in

--- system/desktop/hyprland/modules/hypridle/default.nix
+++ system/desktop/hyprland/modules/hypridle/default.nix
@@ -11,66 +11,63 @@ in
 {
   services.hypridle.enable = true;
 
-  home-manager.users =
+  home-manager.users = mapAttrs (
+    user: _:
     let
+      idle = cfg.system.users.${user}.desktop.idle;
     in
-    mapAttrs (
-      user: _:
-      let
-        idle = cfg.system.users.${user}.desktop.idle;
-      in
-      {
-        home.file.".config/hypr/hypridle.conf".text = ''
-          general {
-              lock_cmd = pidof hyprlock || hyprlock
-              before_sleep_cmd = loginctl lock-session
-              after_sleep_cmd = hyprctl dispatch dpms on && xrandr --output "${cfg.desktop.hyprland.mainMonitor}" --primary
-          }
+    {
+      home.file.".config/hypr/hypridle.conf".text = ''
+        general {
+            lock_cmd = pidof hyprlock || hyprlock
+            before_sleep_cmd = loginctl lock-session
+            after_sleep_cmd = hyprctl dispatch dpms on && xrandr --output "${cfg.desktop.hyprland.mainMonitor}" --primary
+        }
 
-          # Lower brightness
-          listener {
-              timeout = ${builtins.toString (cfg.desktop.hyprland.lock.secondsToLowerBrightness)}
-              on-timeout = brightnessctl -s set 10 && brightnessctl -sd rgb:kbd_backlight set 0
-              on-resume = brightnessctl -r && brightnessctl -rd rgb:kbd_backlight
-          }
+        # Lower brightness
+        listener {
+            timeout = ${builtins.toString (cfg.desktop.hyprland.lock.secondsToLowerBrightness)}
+            on-timeout = brightnessctl -s set 10 && brightnessctl -sd rgb:kbd_backlight set 0
+            on-resume = brightnessctl -r && brightnessctl -rd rgb:kbd_backlight
+        }
 
-          ${
-            if (idle.lock.enable) then
-              ''
-                listener {
-                    timeout = ${builtins.toString (idle.lock.seconds)}
-                    on-timeout = hyprlock-wrapper lock
-                }
-              ''
-            else
-              ""
-          }
+        ${
+          if (idle.lock.enable) then
+            ''
+              listener {
+                  timeout = ${builtins.toString (idle.lock.seconds)}
+                  on-timeout = hyprlock-wrapper lock
+              }
+            ''
+          else
+            ""
+        }
 
-          ${
-            if (idle.disableMonitors.enable) then
-              ''
-                listener {
-                    timeout = ${builtins.toString (idle.disableMonitors.seconds)}
-                    on-timeout = hyprlock-wrapper off
-                    on-resume = hyprctl dispatch dpms on
-                }
-              ''
-            else
-              ""
-          }
+        ${
+          if (idle.disableMonitors.enable) then
+            ''
+              listener {
+                  timeout = ${builtins.toString (idle.disableMonitors.seconds)}
+                  on-timeout = hyprlock-wrapper off
+                  on-resume = hyprctl dispatch dpms on
+              }
+            ''
+          else
+            ""
+        }
 
-          ${
-            if (idle.suspend.enable) then
-              ''
-                listener {
-                    timeout = ${builtins.toString (idle.suspend.seconds)}
-                    on-timeout = hyprlock-wrapper suspend
-                }
-              ''
-            else
-              ""
-          }
-        '';
-      }
-    ) cfg.system.users;
+        ${
+          if (idle.suspend.enable) then
+            ''
+              listener {
+                  timeout = ${builtins.toString (idle.suspend.seconds)}
+                  on-timeout = hyprlock-wrapper suspend
+              }
+            ''
+          else
+            ""
+        }
+      '';
+    }
+  ) cfg.system.users;
 }
