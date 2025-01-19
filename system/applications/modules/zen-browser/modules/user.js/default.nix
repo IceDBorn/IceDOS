0bd2a7de8159d48e08fd6f66ec96acee5a2c9448
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-19 17:09:42 +0200

    zen-browser: update default styling and settings

--- system/applications/modules/zen-browser/modules/user.js/default.nix
+++ system/applications/modules/zen-browser/modules/user.js/default.nix
@@ -12,42 +12,29 @@ let
   firefoxVersion = builtins.substring 0 5 pkgs.firefox.version;
 
   userJs = ''
-    // Global settings
-    user_pref("app.update.auto", false);
-    user_pref("browser.download.useDownloadDir", true);
-    user_pref("browser.ml.chat.enabled", true);
-    user_pref("browser.ml.chat.provider", "https://huggingface.co/chat");
+    user_pref("browser.download.always_ask_before_handling_new_types", false);
     user_pref("browser.newtabpage.enabled", false);
     user_pref("browser.search.separatePrivateDefault", false);
+    user_pref("browser.shell.checkDefaultBrowser", false);
     user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
-    user_pref("browser.theme.dark-private-windows", false);
     user_pref("browser.toolbars.bookmarks.visibility", "always");
-    user_pref("browser.urlbar.suggest.clipboard", false);
-    user_pref("browser.urlbar.suggest.engines", false);
-    user_pref("browser.urlbar.suggest.history", false);
-    user_pref("browser.urlbar.suggest.openpage", false);
-    user_pref("browser.urlbar.suggest.recentsearches", false);
-    user_pref("browser.urlbar.suggest.topsites", false);
-    user_pref("devtools.debugger.features.windowless-service-workers", true);
     user_pref("dom.webgpu.enabled", true);
     user_pref("general.autoScroll", true);
-    user_pref("general.smoothScroll.msdPhysics.enabled", true);
     user_pref("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64; rv:${firefoxVersion}) Gecko/20100101 Firefox/${firefoxVersion}");
-    user_pref("image.jxl.enabled", true); // Enable JPEG XL support
+    user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
     user_pref("middlemouse.paste", false);
     user_pref("mousewheel.default.delta_multiplier_x", 250);
-    user_pref("mousewheel.default.delta_multiplier_y", 250);
     user_pref("mousewheel.with_shift.delta_multiplier_y", 250);
-    user_pref("permissions.default.desktop-notification", 0);
-    user_pref("privacy.sanitize.sanitizeOnShutdown", true);
-    user_pref("svg.context-properties.content.enabled", true);
-    user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
     user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
-    user_pref("zen.tabs.show-newtab-vertical", false);
+    user_pref("zen.splitView.change-on-hover", true);
     user_pref("zen.theme.accent-color", "${accentColor}");
     user_pref("zen.theme.color-prefs.amoled", true);
-    user_pref("zen.view.sidebar-expanded", false);
-    user_pref("zen.view.sidebar-expanded.on-hover", true);
+    user_pref("zen.theme.color-prefs.use-workspace-colors", false);
+    user_pref("zen.urlbar.behavior", "float");
+    user_pref("zen.view.compact", true);
+    user_pref("zen.view.show-newtab-button-border-top", false);
+    user_pref("zen.view.sidebar-expanded.on-hover", false);
+    user_pref("zen.welcome-screen.seen", true);
 
     ${
       if (!cfg.applications.zen-browser.privacy) then
@@ -58,7 +45,13 @@ let
       else
         ''
           user_pref("browser.startup.page", 1);
-          user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+          user_pref("browser.urlbar.suggest.history", false);
+          user_pref("browser.urlbar.suggest.recentsearches", false);
+          user_pref("pref.privacy.disable_button.cookie_exceptions", false);
+          user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
+          user_pref("privacy.history.custom", true);
+          user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+          user_pref("signon.management.page.breach-alerts.enabled", false);
           user_pref("signon.rememberSignons", false);
         ''
     }
@@ -81,7 +74,6 @@ mkIf (cfg.applications.zen-browser.enable) {
       if (cfg.applications.zen-browser.pwas.enable) then
         userJs
         + ''
-          user_pref("zen.view.sidebar-expanded.on-hover", false);
           user_pref("browser.toolbars.bookmarks.visibility", "never");
         ''
       else
