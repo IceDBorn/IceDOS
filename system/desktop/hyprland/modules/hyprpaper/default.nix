94482a5613f3a7063b4d253d7c50d28ff94e6af9
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-20 20:17:27 +0200

    hyprpaper: use home manager module

--- system/desktop/hyprland/modules/hyprpaper/default.nix
+++ system/desktop/hyprland/modules/hyprpaper/default.nix
@@ -14,26 +14,15 @@ in
   environment.systemPackages = [ package ];
 
   home-manager.users = mapAttrs (user: _: {
-    home.file = {
-      ".config/hypr/hyprpaper.conf".text = ''
-        preload = ~/.config/hypr/hyprpaper.jpg
-        wallpaper = , ~/.config/hypr/hyprpaper.jpg
-        ipc = off
-      '';
+    home.file.".config/hypr/hyprpaper.jpg".source = ./hyprpaper.jpg;
 
-      ".config/hypr/hyprpaper.jpg".source = ./hyprpaper.jpg;
-    };
+    services.hyprpaper = {
+      enable = true;
 
-    systemd.user.services.hyprpaper = {
-      Unit.Description = "Hyprpaper - Wallpaper Manager";
-      Install.WantedBy = [ "graphical-session.target" ];
-
-      Service = {
-        ExecStart = "${package}/bin/hyprpaper";
-        Nice = "-20";
-        Restart = "on-failure";
-        StartLimitIntervalSec = 60;
-        StartLimitBurst = 60;
+      settings = {
+        preload = "~/.config/hypr/hyprpaper.jpg";
+        wallpaper = ", ~/.config/hypr/hyprpaper.jpg";
+        ipc = "off";
       };
     };
   }) cfg.system.users;
