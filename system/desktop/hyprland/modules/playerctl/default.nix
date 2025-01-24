0729374234dc8dd9c9c5b32d54b1e2c64e7a4ec5
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-24 03:28:31 +0200

    playerctl: introduce module

---
+++ system/desktop/hyprland/modules/playerctl/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mapAttrs;
+  cfg = config.icedos;
+in
+{
+  environment.systemPackages = [ pkgs.playerctl ];
+
+  home-manager.users = mapAttrs (user: _: {
+    wayland.windowManager.hyprland.settings.bindl = [
+      ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioPrev, exec, playerctl previous"
+      ", XF86AudioNext, exec, playerctl next"
+    ];
+  }) cfg.system.users;
+}
