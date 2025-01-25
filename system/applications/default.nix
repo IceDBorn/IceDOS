cb3d470e8981b9ac050fd3d2a45529eb07197086
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-25 21:02:47 +0200

    packages: move global ones into default module

--- system/applications/default.nix
+++ system/applications/default.nix
@@ -23,13 +23,26 @@ let
       )
     );
 
-  pkgFile = lib.importTOML ./packages.toml;
-
   pkgMapper =
     pkgList: lists.map (pkgName: foldl' (acc: cur: acc.${cur}) pkgs (splitString "." pkgName)) pkgList;
 in
 {
   imports = getModules (./modules);
+
   environment.systemPackages =
-    (pkgMapper pkgFile.packages) ++ (pkgMapper cfg.applications.extraPackages);
+    with pkgs;
+    [
+      efibootmgr # Edit EFI entries
+      killall # Tool to kill all programs matching process name
+      lazygit # Git TUI
+      ncdu # Terminal disk analyzer
+      nix-health # Check system health
+      nixfmt-rfc-style # A nix formatter
+      ntfs3g # Support NTFS drives
+      p7zip # 7zip
+      unrar # Support opening rar files
+      unzip # An extraction utility
+      wget # Terminal downloader
+    ]
+    ++ (pkgMapper cfg.applications.extraPackages);
 }
