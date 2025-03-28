265f49b5a0397a2aecab391e28c9954a3b87d6e4
IceDBorn <git.outsider841@simplelogin.fr>
2025-03-29 01:41:42 +0200

    steam: add script to launch games using gamescope without frametiming issues after ~24 minutes

--- system/applications/modules/steam/default.nix
+++ system/applications/modules/steam/default.nix
@@ -6,8 +6,97 @@
 }:
 
 let
-  inherit (lib) mapAttrs mkIf;
+  inherit (lib)
+    head
+    last
+    mapAttrs
+    mkIf
+    splitString
+    ;
+
   cfg = config.icedos;
+
+  gamescope-launch = (
+    pkgs.writeShellScriptBin "gamescope-launch" ''
+      OG_LD_PRELOAD="$LD_PRELOAD"
+      LD_PRELOAD=""
+      GAMEMODE="gamemoderun"
+
+      ${
+        if (cfg.applications.mangohud.enable) then
+          ''
+            MANGOHUD="--mangoapp"
+            MANGOHUD_CONFIGFILE="/home/$USER/.config/MangoHud/MangoHud.conf"
+          ''
+        else
+          ""
+      }
+
+      ${
+        let
+          monitor = head (cfg.hardware.monitors);
+          resolution = splitString "x" (monitor.resolution);
+          width = head (resolution);
+          height = last (resolution);
+          refreshRate = toString (monitor.refreshRate);
+        in
+        ''
+          DEFAULT_WIDTH="-W ${width}"
+          DEFAULT_HEIGHT="-H ${height}"
+          DEFAULT_REFRESH_RATE="-r ${refreshRate}"
+        ''
+      }
+
+      while [[ $# -gt 0 ]]; do
+        case "$1" in
+          --gamescope-args)
+            GAMESCOPE_ARGS="$2"
+
+            if [[ $GAMESCOPE_ARGS = *'-W'* ]] || [[ $GAMESCOPE_ARGS = *'-H'* ]]; then
+                DEFAULT_WIDTH=""
+                DEFAULT_HEIGHT=""
+            fi
+
+            if [[ $GAMESCOPE_ARGS = *'-r'* ]]; then
+                DEFAULT_REFRESH_RATE=""
+            fi
+
+            shift 2
+            ;;
+          --no-gamemode)
+            GAMEMODE=""
+            shift
+            ;;
+          --no-mangohud)
+            MANGOHUD=""
+            shift
+            ;;
+          --)
+            shift
+            COMMAND=("$@")
+            break
+            ;;
+          *)
+            COMMAND=("$@")
+            break
+            ;;
+          -*|--*)
+            echo "Unknown option: $1" >&2
+            exit 1
+            ;;
+        esac
+      done
+
+      DEFAULT_GAMESCOPE_ARGS="$DEFAULT_WIDTH $DEFAULT_HEIGHT $DEFAULT_REFRESH_RATE"
+
+      $GAMEMODE gamescope $MANGOHUD $DEFAULT_GAMESCOPE_ARGS $GAMESCOPE_ARGS -- env LD_PRELOAD="$OG_LD_PRELOAD" "''${COMMAND[@]}"
+    ''
+  );
+
+  extraPackages = [
+    pkgs.gamescope
+    gamescope-launch
+  ];
 in
 mkIf (cfg.applications.steam.enable) {
   home-manager.users = mapAttrs (
@@ -37,7 +126,9 @@ mkIf (cfg.applications.steam.enable) {
         packages =
           with pkgs;
           mkIf (!cfg.hardware.devices.steamdeck) [
-            (steam.override { extraPkgs = pkgs: [ pkgs.gamescope ]; })
+            (steam.override {
+              extraPkgs = pkgs: extraPackages;
+            })
           ];
       };
     }
@@ -46,6 +137,6 @@ mkIf (cfg.applications.steam.enable) {
   programs.steam = mkIf (cfg.hardware.devices.steamdeck) {
     enable = true;
     extest.enable = true;
-    extraPackages = [ pkgs.gamescope ];
+    extraPackages = extraPackages;
   };
 }
