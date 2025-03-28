411b02e30a1cc1ffba3bcbf88b5910923369f7bc
jim3692 <31220180+jim3692@users.noreply.github.com>
2025-03-28 16:16:42 +0200

    virt-manager: enable pcie passthrough (#274)
    * virt-manager: enable pcie passthrough
    
    * virt-manager: change if/else to lib.optional

--- system/applications/modules/virt-manager/default.nix
+++ system/applications/modules/virt-manager/default.nix
@@ -5,7 +5,8 @@
 }:
 
 let
-  inherit (lib) mapAttrs mkIf;
+  inherit (lib) mapAttrs mkIf optional;
+  inherit (config.icedos.hardware) cpus;
   cfg = config.icedos.system;
 in
 mkIf (cfg.virtualisation.virtManager) {
@@ -19,4 +20,12 @@ mkIf (cfg.virtualisation.virtManager) {
   users.users = mapAttrs (user: _: {
     extraGroups = [ "libvirtd" ];
   }) cfg.users;
+
+  boot.kernelParams = [
+    # Allows passthrough of independent devices, that are members of larger IOMMU groups
+    # It only affects kernels with ACS Override support. Ex: CachyOS, Liquorix, Zen
+    "pcie_acs_override=downstream,multifunction"
+  ]
+    ++ optional cpus.amd.enable "amd_iommu=on"
+    ++ optional cpus.intel "intel_iommu=on";
 }
