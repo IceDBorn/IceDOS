be2d82f9b22e137797465ba43caa5c0d658934f3
IceDBorn <git.outsider841@simplelogin.fr>
2025-04-05 03:46:34 +0300

    vpn-exclude: move into icedos toolset

--- system/applications/modules/vpn-exclude/default.nix
+++ system/applications/modules/vpn-exclude/default.nix
@@ -8,28 +8,43 @@ let
   cfg = config.icedos.hardware.networking;
 in
 {
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "vpn-exclude" ''
-      INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
-      GATEWAY=$(ip -o -4 route show to default | awk '{print $3}')
-      NAMESPACE="vpnexcludens"
-      LINK="vpnexcludelink"
+  icedos.internals.toolset.commands = [
+    (
+      let
+        command = "vpn-exclude";
+      in
+      {
+        bin = "${pkgs.writeShellScript command ''
+          if [[ "$1" == "" ]]; then
+            echo "error: provide command as an argument"
+            exit 1
+          fi
 
-      [ -f "/var/run/netns/$NAMESPACE" ] || sudo bash -c "
-        (
-          ip netns add $NAMESPACE
-          ip link add $LINK link $INTERFACE address 00:11:22:33:44:55 type macvlan mode bridge
-          ip link set $LINK netns $NAMESPACE
+          INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+          GATEWAY=$(ip -o -4 route show to default | awk '{print $3}')
+          NAMESPACE="vpnexcludens"
+          LINK="vpnexcludelink"
 
-          ip netns exec $NAMESPACE ip link set dev lo up
-          ip netns exec $NAMESPACE ip link set dev $LINK up
+          [ -f "/var/run/netns/$NAMESPACE" ] || sudo bash -c "
+            (
+              ip netns add $NAMESPACE
+              ip link add $LINK link $INTERFACE address 00:11:22:33:44:55 type macvlan mode bridge
+              ip link set $LINK netns $NAMESPACE
 
-          ip netns exec $NAMESPACE ip addr add ${cfg.vpnExcludeIp}/24 dev $LINK
-          ip netns exec $NAMESPACE ip route add default via $GATEWAY dev $LINK
-        )
-      "
+              ip netns exec $NAMESPACE ip link set dev lo up
+              ip netns exec $NAMESPACE ip link set dev $LINK up
 
-      sudo ip netns exec "$NAMESPACE" sudo -u "$USER" "$1"
-    '')
+              ip netns exec $NAMESPACE ip addr add ${cfg.vpnExcludeIp}/24 dev $LINK
+              ip netns exec $NAMESPACE ip route add default via $GATEWAY dev $LINK
+            )
+          "
+
+          sudo ip netns exec "$NAMESPACE" sudo -u "$USER" "$1"
+        ''}";
+
+        command = command;
+        help = "run command excluded from all vpn connections";
+      }
+    )
   ];
 }
