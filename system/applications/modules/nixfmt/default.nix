1bf9078fcf4527d9442f8dedc2278131c5077069
IceDBorn <git.outsider841@simplelogin.fr>
2025-04-08 11:45:39 +0300

    nixfmt: move dir script into toolset

--- system/applications/modules/nixfmt/default.nix
+++ system/applications/modules/nixfmt/default.nix
@@ -7,8 +7,18 @@ let
   package = pkgs.nixfmt-rfc-style;
 in
 {
-  environment.systemPackages = [
-    package
-    (pkgs.writeShellScriptBin "nixfmt-dir" ''find "''${1:-.}" -type f -name "*.nix" -exec "${package}/bin/nixfmt" {} \;'')
+  environment.systemPackages = [ package ];
+
+  icedos.internals.toolset.commands = [
+    (
+      let
+        command = "nixf";
+      in
+      {
+        bin = "${pkgs.writeShellScript command ''find "''${1:-.}" -type f -name "*.nix" -exec "${package}/bin/nixfmt" {} \;''}";
+        command = command;
+        help = "format all nix files of current or provided directory";
+      }
+    )
   ];
 }
