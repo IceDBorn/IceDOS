19a22309c872faabf25f44aa4aa4a4d5f889b196
CrazyStevenz <github.ekta@aleeas.com>
2025-04-13 12:02:32 +0300

    gnome: exclude more preinstalled packages

--- system/desktop/gnome/default.nix
+++ system/desktop/gnome/default.nix
@@ -25,15 +25,20 @@ in
     epiphany # Web browser
     evince # Document viewer
     geary # Email
+    gnome-browser-connector # Install gnome extensions from the browser
+    gnome-calendar # Calendar
     gnome-characters # Emojis
     gnome-console # Terminal
+    gnome-contacts # Contacts
     gnome-font-viewer # Font viewer
     gnome-maps # Maps
+    gnome-music # Music
     gnome-software # Software center
     gnome-system-monitor # System monitoring tool
     gnome-text-editor # Text editor
     gnome-tour # Greeter
     simple-scan # Scanner
+    totem # Videos
     yelp # Help
   ];
 }
